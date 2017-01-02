a2e9b5bdf838241619eaaf695e5f73ca3d99e095
sculove <sculove.pe.kr@navercorp.com>
2017-01-02 19:01:02 +0900

    doc: update README.md

--- README.md
+++ README.md
@@ -1,73 +1,88 @@
 # eg.MovableCoord
+A module used to change the information of user action entered by various input devices such as touch screen or mouse into logical coordinates within the virtual coordinate system. The coordinate information sorted by time events occurred is provided if animations are made by user actions. You can implement a user interface by applying the logical coordinates provided. For more information on the eg.MovableCoord module, see demos.
 
-## requirement
 
+* Usage Examples of egjs: http://codepen.io/collection/AKpkGW/
+* API Documentation
+    - Latest: http://naver.github.io/egjs/latest/doc/eg.MovableCoord.html
+    - Specific version: http://naver.github.io/egjs/[VERSION]/doc/eg.MovableCoord.html
+
+## Supported Browsers
+
+The following table shows browsers supported by eg.MovableCoord
+
+|Internet Explorer|Chrome|Firefox|Safari|iOS|Android|
+|---|---|---|---|---|---|
+|10+|Latest|Latest|Latest|7+|2.3+(except 3.x)|
+
+
+## Dependency
+egjs has the dependencies for the following libraries:
+
+|[Hammer.JS](http://hammerjs.github.io/)|
+|----|
+|2.0.4+|
+
+
+Let egjs load after jQuery loads.
+
+```html
+...
+<!-- Load jQuery -->
+<!-- Load egjs packaged with all dependencies (Hammer.js) -->
+<!-- Load from your local installation -->
+<script src="bower_components/egjs/dist/pkgd/eg.pkgd.min.js"></script>
+
+<!-- Or load from CDN -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/egjs/1.3.0/pkgd/eg.pkgd.min.js"></script>
+<div id="area">
+...
 ```
-// dependencies
-npm install
+
+
+```javascript
+// create MovableCoord with option
+var instance = new eg.MovableCoord("#area", {
+  max : [ 300, 400 ]
+});
+
+// call bind method
+instance.bind(el, {
+  direction : eg.MovableCoord.DIRECTION_ALL,
+  scale: [1, 1.5]
+});
+
+// call unbind method
+instance.unbind(el);
 ```
 
-## build
 
-```
-npm run build //or webpack
-```
+## Bug Report
 
-## test
+If you find a bug, please report it to us using the [Issues](https://github.com/naver/eg.movableCoord/issues) page on GitHub.
 
-```
-npm run test //or karma start
-```
-
-## documentation
-
-```
-npm run jsdoc //or jsdoc -c jsdoc.json
-```
-
-## dev server
-
-```
-npm run dev-server //or webpack-dev-server
-```
-
-checkout http://localhost:8080/demo/
-
-## directories
-
-```
-eg.movableCoord
-  ├─ dist/
-  │   └─ eg.movableCoord.js
-  │   └─ eg.movableCoord.min.js
-  │   └─ eg.movableCoord.js.map
-  │   └─ eg.movableCoord.min.js.map
-  ├─ src/
-  │   └─ index.js
-  ├─ demo/
-  │   ├─ js
-  │   │   └─ demo.js
-  │   └─ index.html
-  ├─ doc/
-  ├─ test/
-  │   ├─ manual/
-  │   │   ├─ js
-  │   │   │   └─ test.js
-  │   │   └─ index.html
-  │   └─ unit/
-  │       ├─ js
-  │       │   └─ test.js
-  │       └─ index.html
-  ├─ .editconfig
-  ├─ .gitignore
-  ├─ .npmignore
-  ├─ bower.json
-  ├─ karma.conf.js
-  ├─ package.json
-  ├─ README.md
-  └─ webpack.config.js
-```
 
 ## License
+egjs is released under the [MIT license](http://naver.github.io/egjs/license.txt).
 
-MIT © NAVER Corp.
+```
+Copyright (c) 2015 NAVER Corp.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+```
--- src/movableCoord.js
+++ src/movableCoord.js
@@ -9,6 +9,53 @@ import { Mixin } from "./utils";
 const pos = Symbol("pos");
 const hammerManager = Symbol("hammerManager");
 
+/**
+ * A module used to change the information of user action entered by various input devices such as touch screen or mouse into logical coordinates within the virtual coordinate system. The coordinate information sorted by time events occurred is provided if animations are made by user actions. You can implement a user interface by applying the logical coordinates provided. For more information on the eg.MovableCoord module, see demos.
+ * @group egjs
+ * @ko 터치 입력 장치나 마우스와 같은 다양한 입력 장치로 전달 받은 사용자의 동작을 가상 좌표계의 논리적 좌표로 변경하는 모듈. 사용자의 동작으로 애니메이션이 일어나면 시간순으로 변경되는 좌표 정보도 제공한다. 변경된 논리적 좌표를 반영해 UI를 구현할 수 있다. eg.MovableCoord 모듈의 자세한 작동 방식은 데모를 참고한다.
+ * @class
+ * @name eg.MovableCoord
+ * @extends eg.Component
+ *
+ * @param {Object} options The option object of the eg.MovableCoord module<ko>eg.MovableCoord 모듈의 옵션 객체</ko>
+ * @param {Array} options.min The minimum value of X and Y coordinates <ko>좌표계의 최솟값</ko>
+ * @param {Number} [options.min.0=0] The X coordinate of the minimum <ko>최소 x좌표</ko>
+ * @param {Number} [options.min.1=0] The Y coordinate of the minimum <ko>최소 y좌표</ko>
+ *
+ * @param {Array} options.max The maximum value of X and Y coordinates <ko>좌표계의 최댓값</ko>
+ * @param {Number} [options.max.0=100] The X coordinate of the maximum<ko>최대 x좌표</ko>
+ * @param {Number} [options.max.1=100] The Y coordinate of the maximum<ko>최대 y좌표</ko>
+ *
+ * @param {Array} options.bounce The size of bouncing area. The coordinates can exceed the coordinate area as much as the bouncing area based on user action. If the coordinates does not exceed the bouncing area when an element is dragged, the coordinates where bouncing effects are applied are retuned back into the coordinate area<ko>바운스 영역의 크기. 사용자의 동작에 따라 좌표가 좌표 영역을 넘어 바운스 영역의 크기만큼 더 이동할 수 있다. 사용자가 끌어다 놓는 동작을 했을 때 좌표가 바운스 영역에 있으면, 바운스 효과가 적용된 좌표가 다시 좌표 영역 안으로 들어온다</ko>
+ * @param {Boolean} [options.bounce.0=10] The size of top area <ko>위쪽 바운스 영역의 크기</ko>
+ * @param {Boolean} [options.bounce.1=10] The size of right area <ko>오른쪽 바운스 영역의 크기</ko>
+ * @param {Boolean} [options.bounce.2=10] The size of bottom area <ko>아래쪽 바운스 영역의 크기</ko>
+ * @param {Boolean} [options.bounce.3=10] The size of left area <ko>왼쪽 바운스 영역의 크기</ko>
+ *
+ * @param {Array} options.margin The size of accessible space outside the coordinate area. If an element is dragged outside the coordinate area and then dropped, the coordinates of the element are returned back into the coordinate area. The size of margins that can be exceeded <ko>−	좌표 영역을 넘어 이동할 수 있는 바깥 영역의 크기. 사용자가 좌표를 바깥 영역까지 끌었다가 놓으면 좌표가 좌표 영역 안으로 들어온다.</ko>
+ * @param {Boolean} [options.margin.0=0] The size of top margin <ko>위쪽 바깥 영역의 크기</ko>
+ * @param {Boolean} [options.margin.1=0] The size of right margin <ko>오른쪽 바깥 영역의 크기</ko>
+ * @param {Boolean} [options.margin.2=0] The size of bottom margin <ko>아래쪽 바깥 영역의 크기</ko>
+ * @param {Boolean} [options.margin.3=0] The size of left margin <ko>왼쪽 바깥 영역의 크기</ko>
+ * @param {Array} options.circular Indicates whether a circular element is available. If it is set to "true" and an element is dragged outside the coordinate area, the element will appear on the other side.<ko>순환 여부. 'true'로 설정한 방향의 좌표 영역 밖으로 엘리먼트가 이동하면 반대 방향에서 엘리먼트가 나타난다</ko>
+ * @param {Boolean} [options.circular.0=false] Indicates whether to circulate to top <ko>위로 순환 여부</ko>
+ * @param {Boolean} [options.circular.1=false] Indicates whether to circulate to right <ko>오른쪽으로 순환 여부</ko>
+ * @param {Boolean} [options.circular.2=false] Indicates whether to circulate to bottom  <ko>아래로 순환 여부</ko>
+ * @param {Boolean} [options.circular.3=false] Indicates whether to circulate to left  <ko>왼쪽으로 순환 여부</ko>
+ *
+ * @param {Function} [options.easing=easing.easeOutCubic] The easing function to apply to an animation <ko>애니메이션에 적용할 easing 함수</ko>
+ * @param {Number} [options.maximumDuration=Infinity] Maximum duration of the animation <ko>가속도에 의해 애니메이션이 동작할 때의 최대 좌표 이동 시간</ko>
+ * @param {Number} [options.deceleration=0.0006] Deceleration of the animation where acceleration is manually enabled by user. A higher value indicates shorter running time. <ko>사용자의 동작으로 가속도가 적용된 애니메이션의 감속도. 값이 높을수록 애니메이션 실행 시간이 짧아진다</ko>
+ * @see HammerJS {@link http://hammerjs.github.io}
+ * @see •	Hammer.JS applies specific CSS properties by default when creating an instance (See {@link http://hammerjs.github.io/jsdoc/Hammer.defaults.cssProps.html}). The eg.MovableCoord module removes all default CSS properties provided by Hammer.JS <ko>Hammer.JS는 인스턴스를 생성할 때 기본으로 특정 CSS 속성을 적용한다(참고: @link{http://hammerjs.github.io/jsdoc/Hammer.defaults.cssProps.html}). 특정한 상황에서는 Hammer.JS의 속성 때문에 사용성에 문제가 있을 수 있다. eg.MovableCoord 모듈은 Hammer.JS의 기본 CSS 속성을 모두 제거했다</ko>
+ *
+ * @codepen {"id":"jPPqeR", "ko":"MovableCoord Cube 예제", "en":"MovableCoord Cube example", "collectionId":"AKpkGW", "height": 403}
+ *
+ * @see Easing Functions Cheat Sheet {@link http://easings.net/}
+ * @see If you want to try a different easing function, use the jQuery easing plugin ({@link http://gsgd.co.uk/sandbox/jquery/easing}) or the jQuery UI easing library ({@link https://jqueryui.com/easing}) <ko>다른 easing 함수를 사용하려면 jQuery easing 플러그인({@link http://gsgd.co.uk/sandbox/jquery/easing})이나, jQuery UI easing 라이브러리({@lin https://jqueryui.com/easing})를 사용한다</ko>
+ *
+ * @support {"ie": "10+", "ch" : "latest", "ff" : "latest",  "sf" : "latest", "edge" : "latest", "ios" : "7+", "an" : "2.3+ (except 3.x)"}
+ */
 export default class MovableCoord extends Mixin(Component).with(EventHandler, AnimationHandler) {
 	constructor(options) {
 		super();
