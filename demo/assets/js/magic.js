a30f52e362e6d50591c51817c7ac80de8fc53a8d
Dongbin Kim <bindongkim@gmail.com>
2017-07-12 17:50:51 +0900

    feat: Control the movement of the boards
    They should be gathered when the virtual coordinates go down
    They should be spreaded when the virtual coordinates go up

--- demo/assets/js/magic.js
+++ demo/assets/js/magic.js
@@ -1,5 +1,6 @@
 (function() {
   let xPointer, yPointer;
+  let previousYelem;
   document.getElementById('boardWrapper').addEventListener('mousemove', function(e) {
     xPointer = e.clientX - this.offsetLeft;
     yPointer = e.clientY - (this.offsetTop - window.pageYOffset);
@@ -9,20 +10,23 @@
   const axes = new eg.Axes({
     axis: {
       xElem: {
-        range: [1, 300]
+        range: [1, 500]
       },
       yElem: {
-        range: [1, 300]
+        range: [1, 500]
       }
     },
     deceleration: 0.0024
   })
   .on({
     'change': function(e) {
+      previousYelem = previousYelem ? previousYelem : e.pos.yElem;
+      let yElemMove = ((e.pos.yElem - previousYelem) < 0) ? 'up' : 'down'; 
+      previousYelem = e.pos.yElem;
       let verticalDistance = boards[1].offsetTop - boards[0].offsetTop;
       boards.forEach(function(board, index) {
         board.style.left = xPointer + 'px';
-        if(verticalDistance>=30) {
+        if(yElemMove === 'down' && verticalDistance >= 30) {
           switch (index) {
             case 0:
             case 1:
@@ -33,6 +37,19 @@
             case 4:
             case 5:
               board.style.top = board.offsetTop - (index-2)*(e.pos.yElem/15) + 'px';
+              break;
+          }
+        } else if(yElemMove === 'up' && verticalDistance <= 60) {
+          switch (index) {
+            case 0:
+            case 1:
+            case 2:
+              board.style.top = board.offsetTop - (3-index)*(e.pos.yElem/15) + 'px';
+              break;
+            case 3:
+            case 4:
+            case 5:
+              board.style.top = board.offsetTop + (index-2)*(e.pos.yElem/15) + 'px';
               break;
           }
         }
