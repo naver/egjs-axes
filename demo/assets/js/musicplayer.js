08472f0afd412ec1c6d17569d3f5ab6b721215a6
doyoon.kim <doyoon.kim@navercorp.com>
2017-07-12 15:47:09 +0900

    chore(demo): making progress temp commit

--- demo/assets/js/musicplayer.js
+++ demo/assets/js/musicplayer.js
@@ -30,73 +30,134 @@ class Music {
 }
 
 const samplePlayList = [
-	new Music("My passion is red", "Reddy", 12, "red", "red"),
-	new Music("title2", "Orangy", 14, "orange", "orange"),
-	new Music("title3", "Yello", 9, "yellow", "yellow"),
-	new Music("title3", "Greenist", 13, "green", "green"),
-	new Music("title3", "Blues", 14, "blue", "blue"),
-	new Music("title3", "Naver", 10, "navy", "navy"),
-	new Music("title3", "Pupler", 8, "purple", "purple"),
+	new Music("My passion is red", "Reddy", 4, "red", "red"),
+	new Music("title2", "Orangy", 5, "orange", "orange"),
+	new Music("title3", "Yello", 3, "yellow", "yellow"),
+	new Music("title3", "Greenist", 4, "green", "green"),
+	new Music("title3", "Blues", 5, "blue", "blue"),
+	new Music("title3", "Naver", 4, "navy", "navy"),
+	new Music("title3", "Pupler", 5, "purple", "purple"),
 ];
 
 class MusicPlayer {
     /**
     *  MusicPlayer class
     *
-    *  @param {object} playList
+    *  @param {Object} playList
     */
 	constructor(playList) {
 		this.currentMusicIndex = 0;
+        this.currentMusicProgress = 0;
+        this.playStatus = false;
 		this.playList = samplePlayList;
+        this.timer = null;
+
 	    // for Dom controll
 		this.turnTableDom = document.getElementById("turntable");
 		this.listContainerDom = document.getElementById("list_container");
 		this.titleDom = document.getElementById("title");
 		this.artistDom = document.getElementById("artist");
+        this.barPlayDom = document.getElementById("bar_play");
 	}
 
 	init() {
 		this.currentMusicIndex = 0;
-		this.initPlayListPannel();
-		this.playMusic(this.currentMusicIndex);
+
+        // eventListener declaration 
+        this.turnTableDom.addEventListener("mousedown", (e) => {
+            this.pauseMusic();
+        });
+
+        this.turnTableDom.addEventListener("mouseup", (e) => {
+            this.playMusic();
+        });
+
+        this.initPlayListPannel();
+		this.playMusic();
 	}
 
+    // make list Dom to the bottom of music player
 	initPlayListPannel() {
 	    let pannel = this.playList.map((music, index) => {
 		    return `<figure>` +
                 `<div id="list_cd" style="background-color:` + music.color + `"/>` +
                         `<div id="list_cd_hole"></div>` +
                 `</div>` +
-                `<div>` + music.title + `</div>` +
+                `<div id="list_cd_title">` + music.title + `</div>` +
             '</figure>';
-	});
+	    });
 		this.listContainerDom.innerHTML = pannel.join("");
 	}
 
-    // play music
-	playMusic(index) {
-        // music information setting;
-		this.titleDom.innerHTML = this.playList[index].title;
+    /**
+    *  core of music player
+    *
+    *  @param {Number} index
+    */
+	player(index) { 
+        let autoPlayNext;
+        let progress = this.currentMusicProgress;
+        this.barPlayDom.style.width = progress / this.playList[index].playTime * 100 + "%";
+        this.titleDom.innerHTML = this.playList[index].title;
 		this.artistDom.innerHTML = this.playList[index].artist;
 		this.turnTableDom.style.backgroundColor = this.playList[index].color;
-		setTimeout(() => {
-			this.nextMusic();
-		}, this.playList[index].playTime * 1000);
+
+        // this.timer = setInterval(() => {
+        //     if(this.playStatus === false) {
+	       //      clearTimeout(autoPlayNext);
+        //         clearInterval(timer);
+        //         this.currentMusicProgress  = progress;
+        //         return false;
+        //     } else if (progress === this.playList[index].playTime) {
+        //         clearInterval(timer);
+        //         return false;
+        //     } else {
+        //         ++progress;
+        //         console.log(progress)
+        //         this.barPlayDom.style.width = progress / this.playList[index].playTime * 100 + "%";
+        //     }
+        // }, 1000);
+        this.timer = setInterval(() => {
+            if (progress === this.playList[index].playTime) {
+                clearInterval(this.timer);
+                this.currentMusicProgress = 0;
+                this.nextMusic();
+                return false;
+            } else {
+                ++progress;
+                console.log(progress)
+                this.barPlayDom.style.width = progress / this.playList[index].playTime * 100 + "%";
+            }
+        }, 1000);
+
+  //       autoPlayNext = setTimeout(() => {
+  //           this.currentMusicProgress = 0;
+		//     this.nextMusic();
+  //           return false;
+		// }, (this.playList[index].playTime - progress) * 1000 + 1000);
 	}
 
-    // stop play music
-	stopMusic() {
+    // play player
+    playMusic() {
+        this.playStatus = true;
+        this.player(this.currentMusicIndex);
+    }
 
+    // pause player
+	pauseMusic() {
+        this.playStatus = false;
+        clearInterval(this.timer);
 	}
 
     // play next music
 	nextMusic() {
-		if (this.currentMusicIndex > this.playList.length) {
+        // if last music of list, go to the first music
+		if (this.currentMusicIndex >= this.playList.length - 1) {
 			this.currentMusicIndex = 0;
 		} else {
 			this.currentMusicIndex = ++this.currentMusicIndex;
 		}
-		this.playMusic(this.currentMusicIndex);
+		this.player(this.currentMusicIndex);
 	}
 
     // play previous music
@@ -106,12 +167,22 @@ class MusicPlayer {
 		} else {
 			this.currentMusicIndex = --this.currentMusicIndex;
 		}
-		this.playMusic(this.currentMusicIndex);
+		this.player(this.currentMusicIndex);
 	}
 
-    // handle play time using egjs-axes
-	playTimeHandler() {
+    // handle turn table using egjs-axes
+	turnTableHandler() {
+        let axes = new egAxes({
+            axis: {
+                angle: {
+                    range: [0, 360],
+                    circular: true
+                }
+            },
+            deceleration: 0.1
+        }).on("change", (e) => {
 
+        })
 	}
 }
 
