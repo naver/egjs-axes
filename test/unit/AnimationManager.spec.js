af108cf29c9005bb301d86b410ea8c2187aad694
Son Chan Uk <sculove@gmail.com>
2017-07-17 22:01:33 +0900

    test(all): add test cases (#32)
    ref #25

--- test/unit/AnimationManager.spec.js
+++ test/unit/AnimationManager.spec.js
@@ -1,100 +1,287 @@
-import {
-	AnimationManager
-} from "../../src/AnimationManager";
+import {AnimationManager} from "../../src/AnimationManager";
 import {AxisManager} from "../../src/AxisManager";
 import {InterruptManager} from "../../src/InterruptManager";
 import {EventManager} from "../../src/EventManager";
-
+import Component from "@egjs/component";
 
 describe("AnimationManager", function () {
-	beforeEach(() => {
-		this.options = {
-			axis: {
-				x: {
-					range: [0, 100],
-					bounce: [50, 50],
-					circular: false
-				},
-				y: {
-					range: [0, 200],
-					bounce: [0, 0],
-					circular: false
-				},
-				z: {
-					range: [-100, 200],
-					bounce: [50, 0],
-					circular: true
-				}
-      },
-      deceleration: 0.0001,
-      maximumDuration: 2000
-		};
-    this.inst = new AnimationManager(
-      this.options, 
-      new InterruptManager(this.options), 
-      new EventManager(this.options),
-      new AxisManager(this.options)
-    );
-	});
-	afterEach(() => {});
+  describe("method test", function() {
+    beforeEach(() => {
+      this.options = {
+        axis: {
+          x: {
+            range: [0, 100],
+            bounce: [50, 50],
+            circular: false
+          },
+          y: {
+            range: [0, 200],
+            bounce: [0, 0],
+            circular: false
+          },
+          z: {
+            range: [-100, 200],
+            bounce: [50, 0],
+            circular: true
+          }
+        },
+        deceleration: 0.0001,
+        maximumDuration: 2000
+      };
+      this.inst = new AnimationManager(
+        this.options, 
+        new InterruptManager(this.options), 
+        new EventManager(null),
+        new AxisManager(this.options)
+      );
+    });
+    afterEach(() => {
+    });
 
-	it("should check 'getDuration' method", () => {
-		// Given
-		// When
-		const depaPos = {
-			x: 0,
-			y: 0
-		};
-		const destPos = {
-			x: 100,
-			y: 50
-		};
+    it("should check 'getDuration' method", () => {
+      // Given
+      // When
+      const depaPos = {
+        x: 0,
+        y: 0
+      };
+      const destPos = {
+        x: 100,
+        y: 50
+      };
 
-    // When/Then
-    expect(this.inst.getDuration(depaPos, destPos)).to.be.equal(1414.213562373095);
+      // When/Then
+      expect(this.inst.getDuration(depaPos, destPos)).to.be.equal(1414.213562373095);
 
-    // When (change option)
-    this.options.maximumDuration = 1200;
+      // When (change option)
+      this.options.maximumDuration = 1200;
 
-    // Then
-    expect(this.inst.getDuration(depaPos, destPos)).to.be.equal(1200);
+      // Then
+      expect(this.inst.getDuration(depaPos, destPos)).to.be.equal(1200);
 
-    // When/Then
-    expect(this.inst.getDuration(depaPos, destPos, 1000)).to.be.equal(1000);
+      // When/Then
+      expect(this.inst.getDuration(depaPos, destPos, 1000)).to.be.equal(1000);
 
-    // When/Then
-    expect(this.inst.getDuration(depaPos, destPos, 2000)).to.be.equal(1200);
+      // When/Then
+      expect(this.inst.getDuration(depaPos, destPos, 2000)).to.be.equal(1200);
+    });
+    
+    it("should check 'createAnimationParam' method", () => {
+      // Given
+      // When
+      const pos = {
+        x: 200,
+        y: 210,
+        z: -155
+      };
+
+      // When
+      let param = this.inst.createAnimationParam(pos, 1000);
+
+      // Then
+      expect(param.depaPos).to.be.eql({x:0, y:0, z: -100});
+      expect(param.destPos).to.be.eql({x:150, y:200, z: -150});
+      expect(param.duration).to.be.eql(1000);
+      expect(param.distance).to.be.eql({x:150, y:200, z:-50});
+      expect(param.inputEvent).to.be.eql(null);
+
+      // When
+      this.options.maximumDuration = 500;
+      const eventValue = { event: "i'm inputEvent"};
+      param = this.inst.createAnimationParam(pos, 1000, eventValue);
+
+      // Then
+      expect(param.depaPos).to.be.eql({x:0, y:0, z: -100});
+      expect(param.destPos).to.be.eql({x:150, y:200, z: -150});
+      expect(param.duration).to.be.eql(500);
+      expect(param.distance).to.be.eql({x:150, y:200, z:-50});
+      expect(param.inputEvent).to.be.equal(eventValue);
+    });
   });
   
-	it("should check 'createAnimationParam' method", () => {
-		// Given
-		// When
-		const pos = {
-			x: 200,
-      y: 210,
-      z: -155
-		};
+  describe("animation test", function() {
+    beforeEach(() => {
+      this.options = {
+        axis: {
+          x: {
+            range: [0, 100],
+            bounce: [50, 50],
+            circular: false
+          },
+          y: {
+            range: [0, 200],
+            bounce: [0, 0],
+            circular: false
+          },
+          z: {
+            range: [-100, 200],
+            bounce: [50, 0],
+            circular: true
+          }
+        },
+        easing: function easeOutCubic(x) {
+          return 1 - Math.pow(1 - x, 3);
+        },
+        interruptable: true,
+        deceleration: 0.0001,
+        maximumDuration: 2000
+      };
+      this.component = new Component();
+      this.inst = new AnimationManager(
+        this.options, 
+        new InterruptManager(this.options), 
+        new EventManager(this.component),
+        new AxisManager(this.options)
+      );
+    });
+    afterEach(() => {
+      this.component.off();
+    });
+    it("should check 'setTo' method (outside)", () => {
+      // Given
+      const depaPos = this.inst.axm.get();
+      const destPos = {
+        x: 200,
+        z: -155
+      };
+      const self = this.inst;
+      const startHandler = sinon.spy();
+      const changeHandler = sinon.spy();
+      const endHandler = sinon.spy();
+      this.component.on({
+        "animationStart": startHandler,
+        "change": changeHandler,
+        "animationEnd": endHandler
+      });
+      
+      // When
+      this.inst.setTo(destPos, 0);
 
-    // When
-    let param = this.inst.createAnimationParam(pos, 1000);
+      // Then
+      expect(startHandler.called).to.be.false;
+      expect(endHandler.called).to.be.false;
+      expect(changeHandler.called).to.be.true;
+      expect(self.axm.get()).to.be.eql({x: 100, y: 0, z:-100});
+    });
+    it("should check 'setTo' method (outside, duration)", (done) => {
+      // Given
+      const depaPos = this.inst.axm.get();
+      const destPos = {
+        x: 200,
+        z: -155
+      };
+      const self = this.inst;
+      const startHandler = sinon.spy();
+      this.component.on({
+        "animationStart": startHandler,
+        "animationEnd": function() {
+          expect(self.axm.get()).to.be.eql({x: 100, y: 0, z:-100});
+          expect(startHandler.callCount).to.be.equal(1);
+          done();
+        }
+      });
+      
+      // When
+      this.inst.setTo(destPos, 1000);
+    });
+    it("should check 'setBy' method (inside, duration)", (done) => {
+      // Given
+      const depaPos = this.inst.axm.get();
+      const byPos = {
+        x: 20,
+        z: 40
+      };
+      const self = this.inst;
+      const startHandler = sinon.spy();
+      this.component.on({
+        "animationStart": startHandler,
+        "animationEnd": function() {
+          expect(self.axm.get()).to.be.eql({x: depaPos.x + byPos.x, y: 0, z:depaPos.z + byPos.z});
+          expect(startHandler.callCount).to.be.equal(1);
+          done();
+        }
+      });
+      
+      // When
+      this.inst.setBy(byPos, 1000);
+    });
+    it("should check 'animateTo' method (inside)", (done) => {
+      // Given
+      const depaPos = this.inst.axm.get();
+      const destPos = {
+        x: 90,
+        z: -80
+      };
+      const self = this.inst;
+      const startHandler = sinon.spy();
+      this.component.on({
+        "animationStart": startHandler,
+        "animationEnd": function() {
+          expect(self.axm.get()).to.be.eql({x: 90, y: 0, z:-80});
+          expect(startHandler.callCount).to.be.equal(1);
+          done();
+        }
+      });
+      
+      // When
+      this.inst.setTo(destPos, 500);
+    });
+    it("should check 'animateTo' method (outside)", (done) => {
+      // Given
+      const depaPos = this.inst.axm.get();
+      const destPos = {
+        x: 200,
+        z: -155
+      };
+      const self = this.inst;
+      const startHandler = sinon.spy();
+      const endHandler = sinon.spy(function() {
+        if(endHandler.callCount === 1) {
+          // first destPos
+          expect(self.axm.get()).to.be.eql({x: 150, y: 0, z:-150});
+          expect(startHandler.callCount).to.be.equal(1);
+        } else if(endHandler.callCount === 2) {
+          // Then
+          expect(self.axm.get()).to.be.eql({x: 100, y: 0, z:-100});
+          expect(startHandler.callCount).to.be.equal(2);
+          done();
+        }
+      });
+      this.component.on({
+        "animationStart": startHandler,
+        "animationEnd": endHandler
+      });
+      
+      // When
+      this.inst.animateTo(destPos, 1000);
+    });
+    it("should check 'animateTo' method (outside. depaPos === destPos => restore)", (done) => {
+      // Given
+      const depaPos = this.inst.axm.get();
+      const destPos = {
+        x: 150,
+        z: -140
+      };
+      this.inst.axm.moveTo(destPos);
+      this.inst.options.maximumDuration = 200;
 
-    // Then
-    expect(param.depaPos).to.be.eql({x:0, y:0, z: -100});
-    expect(param.destPos).to.be.eql({x:150, y:200, z: -150});
-    expect(param.duration).to.be.eql(1000);
-    expect(param.distance).to.be.eql({x:150, y:200, z:-50});
-    expect(param.inputEvent).to.be.eql(null);
+      const startHandler = sinon.spy();
+      const endHandler = sinon.spy();
+      this.component.on({
+        "animationStart": startHandler,
+        "animationEnd": endHandler
+      });
+      
+      // When
+      this.inst.animateTo(destPos, 0);
 
-    // When
-    this.options.maximumDuration = 500;
-    const eventValue = { event: "i'm inputEvent"};
-    param = this.inst.createAnimationParam(pos, 1000, eventValue);
-
-    // Then
-    expect(param.depaPos).to.be.eql({x:0, y:0, z: -100});
-    expect(param.destPos).to.be.eql({x:150, y:200, z: -150});
-    expect(param.duration).to.be.eql(500);
-    expect(param.distance).to.be.eql({x:150, y:200, z:-50});
-    expect(param.inputEvent).to.be.equal(eventValue);
+      // Then
+      setTimeout(() => {
+        expect(startHandler.callCount).to.be.equal(1);
+        expect(endHandler.callCount).to.be.equal(1);
+        expect(this.inst.axm.get()).to.be.eql({x: 100, y: 0, z:-100});
+        done();
+      }, 300);
+    });
   });
 });
--- test/unit/Axes.spec.js
+++ test/unit/Axes.spec.js
@@ -1,67 +1,186 @@
-import Axes from '../../src/Axes.ts';
+import Axes from "../../src/Axes.ts";
+import {HammerInput} from "../../src/inputType/HammerInput.ts";
 
-describe("Axes init Test", function () {
-	beforeEach(() => {
-		this.inst = null;
-	});
-	afterEach(() => {
-		if (this.inst) {
-			this.inst.destroy();
-			this.inst = null;
-		}
-	});
-
-	it("should check a initialization empty value", () => {
-    // Given
-    // When
-    this.inst = new Axes();
-    // Then
-
-    const defaultOptions = {
-      easing: function easeOutCubic(x) {
-        return 1 - Math.pow(1 - x, 3);
-      },
-      interruptable: true,
-      maximumDuration: Infinity,
-      deceleration: 0.0006,
-      axis: {}
-    }
-    
-    expect(this.inst).to.be.exist;
-    expect(defaultOptions.easing(0.5)).to.be.equal(this.inst.options.easing(0.5));
-    expect(defaultOptions.easing(0.3)).to.be.equal(this.inst.options.easing(0.3));
-    expect(defaultOptions.easing(0.1)).to.be.equal(this.inst.options.easing(0.1));
-    expect(defaultOptions.easing(0.7)).to.be.equal(this.inst.options.easing(0.7));
-    expect(defaultOptions.easing(0.9)).to.be.equal(this.inst.options.easing(0.9));
-    expect(defaultOptions.interruptable).to.be.equal(this.inst.options.interruptable);
-    expect(defaultOptions.maximumDuration).to.be.equal(this.inst.options.maximumDuration);
-    expect(defaultOptions.deceleration).to.be.equal(this.inst.options.deceleration);
-    expect(defaultOptions.axis).to.be.eql(this.inst.options.axis);
-	});
-
-  it("should check initialization status", () => {
-    // Given
-    // When
-    this.inst = new Axes({
-      axis: {
-        x: {
-          range: [0, 100],
-          bounce: [30, 50],
-          circular: true
-        },
-        otherX: {
-          range: [-100, 100],
-          bounce: 40,
-          circular: [false, true]
-        }
-      },
-      deceleration: 0.001
+describe("Axes", function () {
+  describe("Axes init Test", function () {
+    beforeEach(() => {
+      this.inst = null;
+    });
+    afterEach(() => {
+      if (this.inst) {
+        this.inst.destroy();
+        this.inst = null;
+      }
     });
 
-    // Then
-    expect(this.inst.options.axis.x.bounce).to.deep.equal([30, 50]);
-    expect(this.inst.options.axis.x.circular).to.deep.equal([true, true]);
-    expect(this.inst.options.axis.otherX.bounce).to.deep.equal([40, 40]);
-    expect(this.inst.options.axis.otherX.circular).to.deep.equal([false, true]);
+    it("should check a initialization empty value", () => {
+      // Given
+      // When
+      this.inst = new Axes();
+      // Then
+
+      const defaultOptions = {
+        easing: function easeOutCubic(x) {
+          return 1 - Math.pow(1 - x, 3);
+        },
+        interruptable: true,
+        maximumDuration: Infinity,
+        deceleration: 0.0006,
+        axis: {}
+      }
+      
+      expect(this.inst).to.be.exist;
+      expect(defaultOptions.easing(0.5)).to.be.equal(this.inst.options.easing(0.5));
+      expect(defaultOptions.easing(0.3)).to.be.equal(this.inst.options.easing(0.3));
+      expect(defaultOptions.easing(0.1)).to.be.equal(this.inst.options.easing(0.1));
+      expect(defaultOptions.easing(0.7)).to.be.equal(this.inst.options.easing(0.7));
+      expect(defaultOptions.easing(0.9)).to.be.equal(this.inst.options.easing(0.9));
+      expect(defaultOptions.interruptable).to.be.equal(this.inst.options.interruptable);
+      expect(defaultOptions.maximumDuration).to.be.equal(this.inst.options.maximumDuration);
+      expect(defaultOptions.deceleration).to.be.equal(this.inst.options.deceleration);
+      expect(defaultOptions.axis).to.be.eql(this.inst.options.axis);
+    });
+
+    it("should check initialization status", () => {
+      // Given
+      // When
+      this.inst = new Axes({
+        axis: {
+          x: {
+            range: [0, 100],
+            bounce: [30, 50],
+            circular: true
+          },
+          otherX: {
+            range: [-100, 100],
+            bounce: 40,
+            circular: [false, true]
+          }
+        },
+        deceleration: 0.001
+      });
+
+      // Then
+      expect(this.inst.options.axis.x.bounce).to.deep.equal([30, 50]);
+      expect(this.inst.options.axis.x.circular).to.deep.equal([true, true]);
+      expect(this.inst.options.axis.otherX.bounce).to.deep.equal([40, 40]);
+      expect(this.inst.options.axis.otherX.circular).to.deep.equal([false, true]);
+    });
+    it("should check `setTo` method", () => {
+      // Given
+      this.inst = new Axes({
+        axis: {
+          x: {
+            range: [0, 100],
+            bounce: [30, 50],
+            circular: true
+          },
+          otherX: {
+            range: [-100, 100],
+            bounce: 40,
+            circular: [false, true]
+          }
+        },
+        deceleration: 0.001
+      });      
+      // When
+      let ret = this.inst.setTo({x: 20});
+
+      // Then
+      expect(this.inst.get()).to.be.eql({x: 20, otherX: -100});
+      expect(ret).to.be.equal(this.inst);
+    });
+
+    it("should check `setTo` method (with duration)", () => {
+      // Given
+      const startHandler = sinon.spy();
+      const changeHandler = sinon.spy();
+      const endHandler = sinon.spy(function() {
+        // Then
+        expect(startHandler.callCount).to.be.equal(1);
+        expect(changeHandler.called).to.be.true;
+        expect(this.inst.get()).to.be.eql({x: 20, otherX: -100});
+        done();
+      });
+      this.inst = new Axes({
+        axis: {
+          x: {
+            range: [0, 100],
+            bounce: [30, 50],
+            circular: true
+          },
+          otherX: {
+            range: [-100, 100],
+            bounce: 40,
+            circular: [false, true]
+          }
+        },
+        deceleration: 0.001
+      }).on({
+        "animationStart": startHandler,
+        "change": changeHandler,
+        "animationEnd": endHandler
+      });
+
+      // When
+      this.inst.setTo({x: 20}, 200);
+    });    
+  });
+
+  describe("Axes Test with InputType", function () {
+    beforeEach(() => {
+      this.inst = new Axes({
+        axis: {
+          x: {
+            range: [0, 100],
+            bounce: [30, 50],
+            circular: true
+          },
+          otherX: {
+            range: [-100, 100],
+            bounce: 40,
+            circular: [false, true]
+          }
+        },
+        deceleration: 0.001
+      });
+      sandbox();
+    });
+    afterEach(() => {
+      if (this.inst) {
+        this.inst.destroy();
+        this.inst = null;
+      }
+      cleanup();
+    });
+
+    it("should check `mapInput` method", () => {
+      // Given
+      const input = new HammerInput("#sandbox");
+
+      // When
+      let ret = this.inst.mapInput("x", input);
+
+      // Then
+      expect(input.axes).to.be.eql(["x"]);
+      expect(ret).to.be.equal(this.inst);
+      input.disconnect();
+
+      // When
+      ret = this.inst.mapInput(["x"], input);
+      
+      // Then
+      expect(input.axes).to.be.eql(["x"]);
+      expect(ret).to.be.equal(this.inst);
+      input.disconnect();
+
+      // When
+      ret = this.inst.mapInput(["x", "y"], input);
+      
+      // Then
+      expect(input.axes).to.be.eql(["x", "y"]);
+      expect(ret).to.be.equal(this.inst);
+      input.disconnect();
+    });
   });
 });
