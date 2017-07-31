658793dfe9c36620a846c181e984ad49aad6a2e2
sculove <sculove@gmail.com>
2017-07-31 20:11:50 +0900

    Updates

--- assets/js/subway.js
+++ assets/js/subway.js
@@ -1,17 +1,15 @@
 (function() {
-  var IMAGE_SIZE = 4500;
-  var wrapper = document.getElementById("zoomWrapper");
-  var wrapperSize = wrapper.getBoundingClientRect().width;
+  const IMAGE_SIZE = 4500;
+  const wrapper = document.getElementById("zoomWrapper");
+  const wrapperSize = wrapper.getBoundingClientRect().width;
   wrapper.style.height = wrapperSize + "px";
-  var imageView = document.getElementById("subway");
-  var imageViewRect = imageView.getBoundingClientRect();
-  var scale = wrapperSize / IMAGE_SIZE;
-  imageView.style[eg.Axes.TRANSFORM] = "scale(" + scale + ")";
+  const imageView = document.getElementById("subway");
+  let imageViewRect = imageView.getBoundingClientRect();
 
-  var xLast = 0;
-  var yLast = 0;
+  const baseScale = wrapperSize / IMAGE_SIZE;
+  const basePos = -(IMAGE_SIZE/(2 * baseScale) - IMAGE_SIZE/2);
 
-  var axes = new eg.Axes({
+  const axes = new eg.Axes({
 		axis: {
 			x: {
         range: [0, 0],
@@ -22,29 +20,32 @@
 				bounce: 50
       },
       zoom: {
-        range: [scale, 1]
+        range: [baseScale, 1]
       }
 		},
-		deceleration: 0.0024
+    minimumDuration: 300
   })
-  .on("release", function(e) {
-    console.info(e.depaPos, "=>", e.destPos);
-  })
-  .on("change", function(e) {
-    // console.log(e);
-    if(e.delta.zoom) {
-      // imageView.style[eg.Axes.TRANSFORM] = "translate(" +  newX + "px, " +  newY + "px) scale(" + e.pos.zoom + ")";
-      // e.set({x: newX, y: newY});
-
-      // change view
-      // imageViewRect = imageView.getBoundingClientRect();
-      // this.options.axis.x.range[1] = this.options.axis.y.range[1] = imageViewRect.width - wrapperSize;
-    } else if(e.delta.x || e.delta.y) {
-      imageView.style[eg.Axes.TRANSFORM] = "translate(" +  (-e.pos.x) + "px, " + (-e.pos.y) + "px) scale(" + e.pos.zoom + ")";
-    }
-  }).connect("zoom", new eg.Axes.WheelInput(wrapper, {
-    scale: Math.abs(scale)
-  })).connect("x y", new eg.Axes.PanInput(imageView, {
+  .on("change", ({pos, delta, inputEvent, set}) => {
+    // if(inputEvent && delta.zoom) {
+    //   console.log("org", inputEvent.layerX,inputEvent.layerY, pos.zoom, "(", delta.zoom, ")");
+    //   // imageView.style[eg.Axes.TRANSFORM] = "translate(" +  newX + "px, " +  newY + "px) scale(" + e.pos.zoom + ")";
+      
+    //   const newX = (inputEvent.layerX + ratio);
+    //   const newY = (inputEvent.layerY * ratio);
+    //   console.log(ratio, ":::" , newX, newY);
+    //   set({x: newX, y: newY});
+    //   imageView.style[eg.Axes.TRANSFORM] = `scale(${pos.zoom}) translate(${-newX}px, ${-newY}px)`;
+    //   imageViewRect = imageView.getBoundingClientRect();
+    //   // change view
+    //   axes.options.axis.x.range[1] = axes.options.axis.y.range[1] = imageViewRect.width - wrapperSize;
+    // } else if(delta.x || delta.y) {
+    imageView.style[eg.Axes.TRANSFORM] = `scale(${pos.zoom}) translate(${-pos.x + basePos}px, ${-pos.y + basePos}px)`;
+    // }
+    
+  });
+  axes.connect("zoom", new eg.Axes.WheelInput(wrapper, {
+    scale: -Math.abs(baseScale)
+  })).connect("x y", new eg.Axes.PanInput(wrapper, {
     scale: [-1, -1]
   }));
 })();
