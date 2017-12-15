d56bfe807aa2d168c72a82843c4aede2a2bf69d6
sculove <sculove@gmail.com>
2017-12-15 14:53:11 +0900

    chore(release): Release 2.3.3

--- dist/axes.js
+++ dist/axes.js
@@ -5,7 +5,7 @@
  * @egjs/axes JavaScript library
  * https://github.com/naver/egjs-axes
  * 
- * @version 2.3.3-rc
+ * @version 2.3.3
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
