4233b25103eb4c925273e3c9c7a0a7e19f44200c
Younkue <younkue.choi@navercorp.com>
2018-01-15 18:02:46 +0900

    feat(Axes): add react-axes

---
+++ framework/react/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Axes from "./src/Axes";
+
+class App extends Component {
+  constructor(prop) {
+    super(prop);
+  }
+  render() {
+    return (<Axes></Axes>);
+  }
+}
+
+export default App;
---
+++ framework/react/src/Axes.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import Axes from "@egjs/axes";
+
+
+export default class Axes extends Component {
+    constructor(props) {
+        this.axes = new Axes();
+    }
+}
