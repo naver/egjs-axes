8dfd20cd088d1f536df31fe70e1fde8541b9055f
Younkue <younkue.choi@navercorp.com>
2018-03-27 20:01:48 +0900

    chore(release): Release 2.5.1

--- outjs/inputType/MoveKeyInput.js
+++ outjs/inputType/MoveKeyInput.js
@@ -22,31 +22,10 @@ exports.KEYMAP = {
 };
 var DIRECTION_REVERSE = -1;
 var DIRECTION_FORWARD = 1;
+var DIRECTION_HORIZONTAL = -1;
+var DIRECTION_VERTICAL = 1;
 var DELAY = 80;
-/**
- * @typedef {Object} MoveKeyInputOption The option object of the eg.Axes.MoveKeyInput module
- * @ko eg.Axes.MoveKeyInput 모듈의 옵션 객체
- * @property {Array<Number>} [scale] Coordinate scale that a user can move<ko>사용자의 동작으로 이동하는 좌표의 배율</ko>
- * @property {Number} [scale[0]=1] Coordinate scale for the first axis<ko>첫번째 축의 배율</ko>
- * @property {Number} [scale[1]=1] Coordinate scale for the decond axis<ko>두번째 축의 배율</ko>
-**/
-/**
- * @class eg.Axes.MoveKeyInput
- * @classdesc A module that passes the amount of change to eg.Axes when the move key stroke is occured. use two axis.
- * @ko 이동키 입력이 발생했을 때의 변화량을 eg.Axes에 전달하는 모듈. 두 개 의 축을 사용한다.
- *
- * @example
- * const moveKey = new eg.Axes.MoveKeyInput("#area", {
- * 		scale: [1, 1]
- * });
- *
- * // Connect 'x', 'y' axes when the moveKey is pressed.
- * axes.connect(["x", "y"], moveKey);
- *
- * @param {HTMLElement|String|jQuery} element An element to use the eg.Axes.MoveKeyInput module <ko>eg.Axes.MoveKeyInput 모듈을 사용할 엘리먼트</ko>
- * @param {MoveKeyInputOption} [options] The option object of the eg.Axes.MoveKeyInput module<ko>eg.Axes.MoveKeyInput 모듈의 옵션 객체</ko>
- */
-var MoveKeyInput = /** @class */ (function () {
+var MoveKeyInput = (function () {
     function MoveKeyInput(el, options) {
         this.axes = [];
         this.element = null;
@@ -65,7 +44,6 @@ var MoveKeyInput = /** @class */ (function () {
     };
     MoveKeyInput.prototype.connect = function (observer) {
         this.dettachEvent();
-        // add tabindex="0" to the container for making it focusable
         if (this.element.getAttribute("tabindex") !== "0") {
             this.element.setAttribute("tabindex", "0");
         }
@@ -76,11 +54,6 @@ var MoveKeyInput = /** @class */ (function () {
         this.dettachEvent();
         return this;
     };
-    /**
-    * Destroys elements, properties, and events used in a module.
-    * @ko 모듈에 사용한 엘리먼트와 속성, 이벤트를 해제한다.
-    * @method eg.Axes.MoveKeyInput#destroy
-    */
     MoveKeyInput.prototype.destroy = function () {
         this.disconnect();
         this.element = null;
@@ -91,36 +64,35 @@ var MoveKeyInput = /** @class */ (function () {
         }
         var isMoveKey = true;
         var direction = DIRECTION_FORWARD;
-        var offsets;
+        var move = DIRECTION_HORIZONTAL;
         switch (e.keyCode) {
             case exports.KEYMAP.LEFT_ARROW:
             case exports.KEYMAP.A:
                 direction = DIRECTION_REVERSE;
+                break;
             case exports.KEYMAP.RIGHT_ARROW:
             case exports.KEYMAP.D:
-                if (!this.axes[0]) {
-                    isMoveKey = false;
-                    break;
-                }
-                offsets = [+this.options.scale[0] * direction, 0];
                 break;
             case exports.KEYMAP.DOWN_ARROW:
             case exports.KEYMAP.S:
                 direction = DIRECTION_REVERSE;
+                move = DIRECTION_VERTICAL;
+                break;
             case exports.KEYMAP.UP_ARROW:
             case exports.KEYMAP.W:
-                if (!this.axes[1]) {
-                    isMoveKey = false;
-                    break;
-                }
-                offsets = [0, +this.options.scale[1] * direction];
+                move = DIRECTION_VERTICAL;
                 break;
             default:
                 isMoveKey = false;
         }
+        if ((move === DIRECTION_HORIZONTAL && !this.axes[0]) ||
+            (move === DIRECTION_VERTICAL && !this.axes[1])) {
+            isMoveKey = false;
+        }
         if (!isMoveKey) {
             return;
         }
+        var offsets = move === DIRECTION_HORIZONTAL ? [+this.options.scale[0] * direction, 0] : [0, +this.options.scale[1] * direction];
         if (!this._isHolded) {
             this.observer.hold(this, event);
             this._isHolded = true;
@@ -153,32 +125,14 @@ var MoveKeyInput = /** @class */ (function () {
         this._isEnabled = false;
         this.observer = null;
     };
-    /**
-     * Enables input devices
-     * @ko 입력 장치를 사용할 수 있게 한다
-     * @method eg.Axes.MoveKeyInput#enable
-     * @return {eg.Axes.MoveKeyInput} An instance of a module itself <ko>모듈 자신의 인스턴스</ko>
-     */
     MoveKeyInput.prototype.enable = function () {
         this._isEnabled = true;
         return this;
     };
-    /**
-     * Disables input devices
-     * @ko 입력 장치를 사용할 수 없게 한다.
-     * @method eg.Axes.MoveKeyInput#disable
-     * @return {eg.Axes.MoveKeyInput} An instance of a module itself <ko>모듈 자신의 인스턴스</ko>
-     */
     MoveKeyInput.prototype.disable = function () {
         this._isEnabled = false;
         return this;
     };
-    /**
-     * Returns whether to use an input device
-     * @ko 입력 장치를 사용 여부를 반환한다.
-     * @method eg.Axes.MoveKeyInput#isEnable
-     * @return {Boolean} Whether to use an input device <ko>입력장치 사용여부</ko>
-     */
     MoveKeyInput.prototype.isEnable = function () {
         return this._isEnabled;
     };
