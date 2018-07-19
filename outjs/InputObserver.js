38d8f6fe44b23594e793b7f4940ecfe2ffd9ca23
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2018-07-19 11:09:49 +0900

    chore(release): Release 2.5.4

---
+++ outjs/InputObserver.js
@@ -0,0 +1,143 @@
+"use strict";
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var AxisManager_1 = require("./AxisManager");
+var Coordinate_1 = __importDefault(require("./Coordinate"));
+var InputObserver = (function () {
+    function InputObserver(_a) {
+        var options = _a.options, itm = _a.itm, em = _a.em, axm = _a.axm, am = _a.am;
+        this.isOutside = false;
+        this.moveDistance = null;
+        this.options = options;
+        this.itm = itm;
+        this.em = em;
+        this.axm = axm;
+        this.am = am;
+    }
+    InputObserver.prototype.atOutside = function (pos) {
+        var _this = this;
+        if (this.isOutside) {
+            return this.axm.map(pos, function (v, k, opt) {
+                var tn = opt.range[0] - opt.bounce[0];
+                var tx = opt.range[1] + opt.bounce[1];
+                return v > tx ? tx : (v < tn ? tn : v);
+            });
+        }
+        else {
+            var initSlope_1 = this.am.easing(0.00001) / 0.00001;
+            return this.axm.map(pos, function (v, k, opt) {
+                var min = opt.range[0];
+                var max = opt.range[1];
+                var out = opt.bounce;
+                if (v < min) {
+                    return min - _this.am.easing((min - v) / (out[0] * initSlope_1)) * out[0];
+                }
+                else if (v > max) {
+                    return max + _this.am.easing((v - max) / (out[1] * initSlope_1)) * out[1];
+                }
+                return v;
+            });
+        }
+    };
+    InputObserver.prototype.get = function (input) {
+        return this.axm.get(input.axes);
+    };
+    InputObserver.prototype.hold = function (input, event) {
+        if (this.itm.isInterrupted() || !input.axes.length) {
+            return;
+        }
+        var changeOption = {
+            input: input,
+            event: event
+        };
+        this.itm.setInterrupt(true);
+        this.am.grab(input.axes, changeOption);
+        !this.moveDistance && this.em.triggerHold(this.axm.get(), changeOption);
+        this.isOutside = this.axm.isOutside(input.axes);
+        this.moveDistance = this.axm.get(input.axes);
+    };
+    InputObserver.prototype.change = function (input, event, offset) {
+        if (!this.itm.isInterrupting() || this.axm.every(offset, function (v) { return v === 0; })) {
+            return;
+        }
+        var depaPos = this.axm.get(input.axes);
+        var destPos;
+        destPos = this.axm.map(this.moveDistance || depaPos, function (v, k) { return v + (offset[k] || 0); });
+        this.moveDistance && (this.moveDistance = destPos);
+        destPos = this.axm.map(destPos, function (v, k, opt) { return Coordinate_1["default"].getCirculatedPos(v, opt.range, opt.circular); });
+        if (this.isOutside &&
+            this.axm.every(depaPos, function (v, k, opt) { return !Coordinate_1["default"].isOutside(v, opt.range); })) {
+            this.isOutside = false;
+        }
+        destPos = this.atOutside(destPos);
+        this.em.triggerChange(destPos, {
+            input: input,
+            event: event
+        }, true);
+    };
+    InputObserver.prototype.release = function (input, event, offset, inputDuration) {
+        if (!this.itm.isInterrupting()) {
+            return;
+        }
+        if (!this.moveDistance) {
+            return;
+        }
+        var pos = this.axm.get(input.axes);
+        var depaPos = this.axm.get();
+        var destPos = this.axm.get(this.axm.map(offset, function (v, k, opt) {
+            if (opt.circular && (opt.circular[0] || opt.circular[1])) {
+                return pos[k] + v;
+            }
+            else {
+                return Coordinate_1["default"].getInsidePosition(pos[k] + v, opt.range, opt.circular, opt.bounce);
+            }
+        }));
+        var duration = this.am.getDuration(destPos, pos, inputDuration);
+        if (duration === 0) {
+            destPos = __assign({}, depaPos);
+        }
+        var param = {
+            depaPos: depaPos,
+            destPos: destPos,
+            duration: duration,
+            delta: this.axm.getDelta(depaPos, destPos),
+            inputEvent: event,
+            input: input,
+            isTrusted: true
+        };
+        this.em.triggerRelease(param);
+        this.moveDistance = null;
+        var userWish = this.am.getUserControll(param);
+        var isEqual = AxisManager_1.AxisManager.equal(userWish.destPos, depaPos);
+        var changeOption = {
+            input: input,
+            event: event
+        };
+        if (isEqual || userWish.duration === 0) {
+            !isEqual && this.em.triggerChange(userWish.destPos, changeOption, true);
+            this.itm.setInterrupt(false);
+            if (this.axm.isOutside()) {
+                this.am.restore(changeOption);
+            }
+            else {
+                this.em.triggerFinish(true);
+            }
+        }
+        else {
+            this.am.animateTo(userWish.destPos, userWish.duration, changeOption);
+        }
+    };
+    return InputObserver;
+}());
+exports.InputObserver = InputObserver;
+;
---
+++ outjs/inputType/PinchInput.js
@@ -0,0 +1,151 @@
+"use strict";
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+exports.__esModule = true;
+var Hammer = __importStar(require("hammerjs"));
+var utils_1 = require("../utils");
+var InputType_1 = require("./InputType");
+var PinchInput = (function () {
+    function PinchInput(el, options) {
+        this.axes = [];
+        this.hammer = null;
+        this.element = null;
+        this._base = null;
+        this._prev = null;
+        this.pinchRecognizer = null;
+        if (typeof Hammer === "undefined") {
+            throw new Error("The Hammerjs must be loaded before eg.Axes.PinchInput.\nhttp://hammerjs.github.io/");
+        }
+        this.element = utils_1.$(el);
+        this.options = __assign({
+            scale: 1,
+            threshold: 0,
+            inputType: ["touch", "pointer"],
+            hammerManagerOptions: {
+                cssProps: {
+                    userSelect: "none",
+                    touchSelect: "none",
+                    touchCallout: "none",
+                    userDrag: "none"
+                }
+            }
+        }, options);
+        this.onPinchStart = this.onPinchStart.bind(this);
+        this.onPinchMove = this.onPinchMove.bind(this);
+        this.onPinchEnd = this.onPinchEnd.bind(this);
+    }
+    PinchInput.prototype.mapAxes = function (axes) {
+        this.axes = axes;
+    };
+    PinchInput.prototype.connect = function (observer) {
+        var hammerOption = { threshold: this.options.threshold };
+        if (this.hammer) {
+            this.removeRecognizer();
+            this.dettachEvent();
+        }
+        else {
+            var keyValue = this.element[InputType_1.UNIQUEKEY];
+            if (!keyValue) {
+                keyValue = String(Math.round(Math.random() * new Date().getTime()));
+            }
+            var inputClass = InputType_1.convertInputType(this.options.inputType);
+            if (!inputClass) {
+                throw new Error("Wrong inputType parameter!");
+            }
+            this.hammer = InputType_1.createHammer(this.element, __assign({
+                inputClass: inputClass
+            }, this.options.hammerManagerOptions));
+            this.element[InputType_1.UNIQUEKEY] = keyValue;
+        }
+        this.pinchRecognizer = new Hammer.Pinch(hammerOption);
+        this.hammer.add(this.pinchRecognizer);
+        this.attachEvent(observer);
+        return this;
+    };
+    PinchInput.prototype.disconnect = function () {
+        this.removeRecognizer();
+        if (this.hammer) {
+            this.hammer.remove(this.pinchRecognizer);
+            this.pinchRecognizer = null;
+            this.dettachEvent();
+        }
+        return this;
+    };
+    PinchInput.prototype.destroy = function () {
+        this.disconnect();
+        if (this.hammer && this.hammer.recognizers.length === 0) {
+            this.hammer.destroy();
+        }
+        delete this.element[InputType_1.UNIQUEKEY];
+        this.element = null;
+        this.hammer = null;
+    };
+    PinchInput.prototype.removeRecognizer = function () {
+        if (this.hammer && this.pinchRecognizer) {
+            this.hammer.remove(this.pinchRecognizer);
+            this.pinchRecognizer = null;
+        }
+    };
+    PinchInput.prototype.onPinchStart = function (event) {
+        this._base = this.observer.get(this)[this.axes[0]];
+        var offset = this.getOffset(event.scale);
+        this.observer.hold(this, event);
+        this.observer.change(this, event, InputType_1.toAxis(this.axes, [offset]));
+        this._prev = event.scale;
+    };
+    PinchInput.prototype.onPinchMove = function (event) {
+        var offset = this.getOffset(event.scale, this._prev);
+        this.observer.change(this, event, InputType_1.toAxis(this.axes, [offset]));
+        this._prev = event.scale;
+    };
+    PinchInput.prototype.onPinchEnd = function (event) {
+        var offset = this.getOffset(event.scale, this._prev);
+        this.observer.change(this, event, InputType_1.toAxis(this.axes, [offset]));
+        this.observer.release(this, event, InputType_1.toAxis(this.axes, [0]), 0);
+        this._base = null;
+        this._prev = null;
+    };
+    PinchInput.prototype.getOffset = function (pinchScale, prev) {
+        if (prev === void 0) { prev = 1; }
+        return this._base * (pinchScale - prev) * this.options.scale;
+    };
+    PinchInput.prototype.attachEvent = function (observer) {
+        this.observer = observer;
+        this.hammer.on("pinchstart", this.onPinchStart)
+            .on("pinchmove", this.onPinchMove)
+            .on("pinchend", this.onPinchEnd);
+    };
+    PinchInput.prototype.dettachEvent = function () {
+        this.hammer.off("pinchstart", this.onPinchStart)
+            .off("pinchmove", this.onPinchMove)
+            .off("pinchend", this.onPinchEnd);
+        this.observer = null;
+        this._prev = null;
+    };
+    PinchInput.prototype.enable = function () {
+        this.hammer && (this.hammer.get("pinch").options.enable = true);
+        return this;
+    };
+    PinchInput.prototype.disable = function () {
+        this.hammer && (this.hammer.get("pinch").options.enable = false);
+        return this;
+    };
+    PinchInput.prototype.isEnable = function () {
+        return !!(this.hammer && this.hammer.get("pinch").options.enable);
+    };
+    return PinchInput;
+}());
+exports.PinchInput = PinchInput;
