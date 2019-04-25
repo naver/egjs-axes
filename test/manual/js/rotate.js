fd8c12270a5a5519dbfee8be1cf1629e111f1826
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2019-04-25 19:46:06 +0900

    test: rotate-paninput manual test
    Ref #126

---
+++ test/manual/js/rotate.js
@@ -0,0 +1,16 @@
+var roulette = document.getElementById("roulette");
+var angleText = document.getElementById("angle-text");
+var input = new eg.Axes.RotatePanInput("#area");
+
+// create eg.Axes with option
+var axes = new eg.Axes({
+  angle: {
+    range: [-720, 720]
+  }
+}).on("change", function(e) {
+  roulette.style.transform="rotate(" + e.pos.angle + "deg)";
+  angleText.textContent = e.pos.angle + "deg";
+});
+
+axes.connect("angle", input);
+axes.setTo({angle: 0});
