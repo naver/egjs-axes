56f2ad7905b58414ba5ea6a0e37da91cd8645853
Jongmoon Yoon <jongmoon.yoon@navercorp.com>
2019-08-16 19:48:47 +0900

    skip: apply review

--- test/unit/inputType/PinchInput.spec.js
+++ test/unit/inputType/PinchInput.spec.js
@@ -1,241 +1,241 @@
 import Hammer from "@egjs/hammerjs";
 import Axes from "../../../src/Axes.ts";
-import {PinchInput} from "../../../src/inputType/PinchInput";
-import {UNIQUEKEY} from "../../../src/inputType/InputType";
+import { PinchInput } from "../../../src/inputType/PinchInput";
+import { UNIQUEKEY } from "../../../src/inputType/InputType";
 
 describe("PinchInput", () => {
-  describe("instance method", function() {
-    beforeEach(() => {
-      this.inst = new PinchInput(sandbox());
-    });
-    afterEach(() => {
-      if (this.inst) {
-        this.inst.destroy();
-        this.inst = null;
-      }
-      cleanup();
-    });
-    it("should check status after disconnect", () => {
-      // Given
-      const beforeHammer = this.inst.hammer;
-      this.inst.connect({});
+	describe("instance method", function () {
+		beforeEach(() => {
+			this.inst = new PinchInput(sandbox());
+		});
+		afterEach(() => {
+			if (this.inst) {
+				this.inst.destroy();
+				this.inst = null;
+			}
+			cleanup();
+		});
+		it("should check status after disconnect", () => {
+			// Given
+			const beforeHammer = this.inst.hammer;
+			this.inst.connect({});
 
-      // When
-      this.inst.disconnect();
+			// When
+			this.inst.disconnect();
 
-      // Then
-      expect(beforeHammer).to.be.not.exist;
-      expect(this.observer).to.be.not.exist;
-      expect(this.inst.element).to.be.exist;
-      expect(UNIQUEKEY in this.inst.element).to.be.true;
-      expect(this._prev).to.be.not.exist;
-    });
-    it("should check status after destroy", () => {
-      // Given
-      this.inst.connect({});
+			// Then
+			expect(beforeHammer).to.be.not.exist;
+			expect(this.observer).to.be.not.exist;
+			expect(this.inst.element).to.be.exist;
+			expect(UNIQUEKEY in this.inst.element).to.be.true;
+			expect(this._prev).to.be.not.exist;
+		});
+		it("should check status after destroy", () => {
+			// Given
+			this.inst.connect({});
 
-      // When
-      this.inst.destroy();
+			// When
+			this.inst.destroy();
 
-      // Then
-      expect(this.inst.hammer).to.be.not.exist;
-      expect(this.inst.element).to.be.not.exist;
-      expect(this.observer).to.be.not.exist;
-      expect(this._prev).to.be.not.exist;
+			// Then
+			expect(this.inst.hammer).to.be.not.exist;
+			expect(this.inst.element).to.be.not.exist;
+			expect(this.observer).to.be.not.exist;
+			expect(this._prev).to.be.not.exist;
 
-      this.inst = null;
-    });
-  });
-  describe("enable/disable", function() {
-    beforeEach(() => {
-      this.el = sandbox();
-      this.inst = new PinchInput(this.el, {inputType: ["touch"]});
-      this.inst.mapAxes(["x"]);
-      this.observer = {
-        get() {
-          return {
-            x: 10
-          }
-        },
-        release() {},
-        hold() {},
-        change() {},
-        options: {
-          deceleration: 0.0001
-        }
-      };
-    });
-    afterEach(() => {
-      if (this.inst) {
-        this.inst.destroy();
-        this.inst = null;
-      }
-      cleanup();
-    });
+			this.inst = null;
+		});
+	});
+	describe("enable/disable", function () {
+		beforeEach(() => {
+			this.el = sandbox();
+			this.inst = new PinchInput(this.el, { inputType: ["touch"] });
+			this.inst.mapAxes(["x"]);
+			this.observer = {
+				get() {
+					return {
+						x: 10
+					}
+				},
+				release() { },
+				hold() { },
+				change() { },
+				options: {
+					deceleration: 0.0001
+				}
+			};
+		});
+		afterEach(() => {
+			if (this.inst) {
+				this.inst.destroy();
+				this.inst = null;
+			}
+			cleanup();
+		});
 
-    it("should check value of `enable/disalbe` methods", () => {
-      // Given
-      // When
-      // Then
-      expect(this.inst.isEnable()).to.be.false;
+		it("should check value of `enable/disalbe` methods", () => {
+			// Given
+			// When
+			// Then
+			expect(this.inst.isEnable()).to.be.false;
 
-      // When
-      this.inst.disable();
+			// When
+			this.inst.disable();
 
-      // Then
-      expect(this.inst.isEnable()).to.be.false;
+			// Then
+			expect(this.inst.isEnable()).to.be.false;
 
-      // When (hammer is not exist)
-      this.inst.enable();
+			// When (hammer is not exist)
+			this.inst.enable();
 
-      // Then
-      expect(this.inst.hammer).to.be.not.exist;
-      expect(this.inst.isEnable()).to.be.false;
+			// Then
+			expect(this.inst.hammer).to.be.not.exist;
+			expect(this.inst.isEnable()).to.be.false;
 
-      // When (hammer is exist)
-      this.inst.connect(this.observer);
-      this.inst.enable();
+			// When (hammer is exist)
+			this.inst.connect(this.observer);
+			this.inst.enable();
 
-      // Then
-      expect(this.inst.hammer).to.be.exist;
-      expect(this.inst.isEnable()).to.be.true;
-    });
-    it("should check event when enable method is called", (done) => {
-      // Given
-      this.inst.connect(this.observer);
-      const beforeHandler = this.inst.hammer.handlers["pinchend"][0];
+			// Then
+			expect(this.inst.hammer).to.be.exist;
+			expect(this.inst.isEnable()).to.be.true;
+		});
+		it("should check event when enable method is called", (done) => {
+			// Given
+			this.inst.connect(this.observer);
+			const beforeHandler = this.inst.hammer.handlers["pinchend"][0];
 
-      // When
-      expect(this.inst.isEnable()).to.be.true;
-      const onPinchEndHandler = sinon.spy(beforeHandler);
-      this.inst.hammer.handlers["pinchend"][0] = onPinchEndHandler;
+			// When
+			expect(this.inst.isEnable()).to.be.true;
+			const onPinchEndHandler = sinon.spy(beforeHandler);
+			this.inst.hammer.handlers["pinchend"][0] = onPinchEndHandler;
 
-      // When
-      Simulator.gestures.pinch(this.el, {
-        duration: 500,
-        scale: 0.5
-      }, () => {
-          // Then
-          expect(onPinchEndHandler.called).to.be.true;
-          this.inst.hammer.handlers["pinchend"][0] = beforeHandler;
-          done();
-      });
-    });
-    it("should check event when disable method is called", (done) => {
-      // Given
-      this.inst.connect(this.observer);
-      const beforeHandler = this.inst.hammer.handlers["pinchend"][0];
-      // When
+			// When
+			Simulator.gestures.pinch(this.el, {
+				duration: 500,
+				scale: 0.5
+			}, () => {
+				// Then
+				expect(onPinchEndHandler.called).to.be.true;
+				this.inst.hammer.handlers["pinchend"][0] = beforeHandler;
+				done();
+			});
+		});
+		it("should check event when disable method is called", (done) => {
+			// Given
+			this.inst.connect(this.observer);
+			const beforeHandler = this.inst.hammer.handlers["pinchend"][0];
+			// When
 
-      const onPinchEndHandler = sinon.spy(beforeHandler);
-      this.inst.hammer.handlers["pinchend"][0] = onPinchEndHandler;
-      expect(this.inst.isEnable()).to.be.true;
-      this.inst.disable();
+			const onPinchEndHandler = sinon.spy(beforeHandler);
+			this.inst.hammer.handlers["pinchend"][0] = onPinchEndHandler;
+			expect(this.inst.isEnable()).to.be.true;
+			this.inst.disable();
 
-      // When
-      Simulator.gestures.pinch(this.el, {
-        duration: 500,
-        scale: 0.5
-      }, () => {
-          // Then
-          expect(onPinchEndHandler.called).to.be.false;
-          this.inst.hammer.handlers["pinchend"][0] = beforeHandler;
-          done();
-      });
-    });
-  });
+			// When
+			Simulator.gestures.pinch(this.el, {
+				duration: 500,
+				scale: 0.5
+			}, () => {
+				// Then
+				expect(onPinchEndHandler.called).to.be.false;
+				this.inst.hammer.handlers["pinchend"][0] = beforeHandler;
+				done();
+			});
+		});
+	});
 
-  describe("offset value", function () {
-    beforeEach(() => {
-      this.el = sandbox();
-      this.input = new PinchInput(this.el, {inputType: ["touch"]});
-      this.inst = new Axes({
-        x: {
-          range: [10, 120]
-        }
-      }, {round: 1}, {
-        x: 50
-      });
-      this.inst.connect(["x"], this.input);
-    });
-    afterEach(() => {
-      if (this.ins) {
-        this.inst.destroy();
-        this.inst = null;
-      }
-      if (this.input) {
-        this.input.destroy();
-        this.input = null;
-      }
-      cleanup();
-    });
+	describe("offset value", function () {
+		beforeEach(() => {
+			this.el = sandbox();
+			this.input = new PinchInput(this.el, { inputType: ["touch"] });
+			this.inst = new Axes({
+				x: {
+					range: [10, 120]
+				}
+			}, { round: 1 }, {
+					x: 50
+				});
+			this.inst.connect(["x"], this.input);
+		});
+		afterEach(() => {
+			if (this.ins) {
+				this.inst.destroy();
+				this.inst = null;
+			}
+			if (this.input) {
+				this.input.destroy();
+				this.input = null;
+			}
+			cleanup();
+		});
 
-    it("The offset value should be returned using the position value when the hold event is triggered.", (done) => {
-      // Given
-      this.input.options.scale = 1;
-      // When
-      Simulator.gestures.pinch(this.el, {
-        duration: 500,
-        scale: 1.1
-      }, () => {
-          // Then
-          expect(this.inst.get(['x']).x).to.be.equal(55);
-          done();
-      });
-    });
+		it("The offset value should be returned using the position value when the hold event is triggered.", (done) => {
+			// Given
+			this.input.options.scale = 1;
+			// When
+			Simulator.gestures.pinch(this.el, {
+				duration: 500,
+				scale: 1.1
+			}, () => {
+				// Then
+				expect(this.inst.get(['x']).x).to.be.equal(55);
+				done();
+			});
+		});
 
-    it("The offset value should apply scale option", (done) => {
-      // Given
-      this.input.options.scale = 1;
-      // When
-      Simulator.gestures.pinch(this.el, {
-        duration: 500,
-        scale: 0.9
-      }, () => {
-					// Then
-          expect(this.inst.get(['x']).x).to.be.equal(45);
-          done();
-      });
-    });
-  });
+		it("The offset value should apply scale option", (done) => {
+			// Given
+			this.input.options.scale = 1;
+			// When
+			Simulator.gestures.pinch(this.el, {
+				duration: 500,
+				scale: 0.9
+			}, () => {
+				// Then
+				expect(this.inst.get(['x']).x).to.be.equal(45);
+				done();
+			});
+		});
+	});
 
-  describe("options test", function() {
-    beforeEach(() => {
-      this.inst = new PinchInput(sandbox());
-    });
-    afterEach(() => {
-      if (this.inst) {
-        this.inst.destroy();
-        this.inst = null;
-      }
-      cleanup();
-    });
-    it("should check hammerManager default Options", () => {
-      // Given
-      expect(this.inst.options.hammerManagerOptions).to.be.eql({
-        cssProps: {
-          userSelect: "none",
-          touchSelect: "none",
-          touchCallout: "none",
-          userDrag: "none",
-        }
-      });
+	describe("options test", function () {
+		beforeEach(() => {
+			this.inst = new PinchInput(sandbox());
+		});
+		afterEach(() => {
+			if (this.inst) {
+				this.inst.destroy();
+				this.inst = null;
+			}
+			cleanup();
+		});
+		it("should check hammerManager default Options", () => {
+			// Given
+			expect(this.inst.options.hammerManagerOptions).to.be.eql({
+				cssProps: {
+					userSelect: "none",
+					touchSelect: "none",
+					touchCallout: "none",
+					userDrag: "none",
+				}
+			});
 
-      // When
-      this.inst.connect({});
+			// When
+			this.inst.connect({});
 
-      // Then
-      expect(this.inst.element.style.userSelect).to.be.equal("none");
-    });
-    it("should check hammerManager Options", () => {
-      // Given
-      this.inst.options.hammerManagerOptions.cssProps.userSelect = "auto";
+			// Then
+			expect(this.inst.element.style.userSelect).to.be.equal("none");
+		});
+		it("should check hammerManager Options", () => {
+			// Given
+			this.inst.options.hammerManagerOptions.cssProps.userSelect = "auto";
 
-      // When
-      this.inst.connect({});
+			// When
+			this.inst.connect({});
 
-      // Then
-      expect(this.inst.element.style.userSelect).to.be.equal("auto");
-    });
-  });
+			// Then
+			expect(this.inst.element.style.userSelect).to.be.equal("auto");
+		});
+	});
 });
--- test/unit/utils.spec.js
+++ test/unit/utils.spec.js
@@ -1,54 +1,54 @@
-import {$, toArray, equal, getDecimalPlace, roundNumber} from "../../src/utils";
+import { $, toArray, equal, getDecimalPlace, roundNumber } from "../../src/utils";
 
-describe("Util Test", function() {
-  beforeEach(() => {
-    this.el = sandbox();
-  });
-  afterEach(() => {
-    cleanup();
-  });
-  it("should check 'equal' method", () => {
-    // Given
-    let target1 = {
-      x: 10,
-      y: 20,
-      z: 30
-    };
-    let target2 = {
-      x: 10,
-      y: 20
-    }
+describe("Util Test", function () {
+	beforeEach(() => {
+		this.el = sandbox();
+	});
+	afterEach(() => {
+		cleanup();
+	});
+	it("should check 'equal' method", () => {
+		// Given
+		let target1 = {
+			x: 10,
+			y: 20,
+			z: 30
+		};
+		let target2 = {
+			x: 10,
+			y: 20
+		}
 
-    // Then
-    expect(equal(target1, target2)).to.be.false;
-    expect(equal(target2, target1)).to.be.true;
-  });
-  it("should check `$` method", () => {
-    // Given
-    // When
-    const complicatedHTML = "<div class='item'><div class='thumbnail'><img class='img-rounded' src='#' /><div class='caption'><p><a href='http://www.naver.com'></a></p></div></div></div>";
-    const div = complicatedHTML; // string
-    const divs = [complicatedHTML, complicatedHTML];
+		// Then
+		expect(equal(target1, target2)).to.be.false;
+		expect(equal(target2, target1)).to.be.true;
+	});
+	it("should check `$` method", () => {
+		// Given
+		// When
+		const complicatedHTML = "<div class='item'><div class='thumbnail'><img class='img-rounded' src='#' /><div class='caption'><p><a href='http://www.naver.com'></a></p></div></div></div>";
+		const div = complicatedHTML; // string
+		const divs = [complicatedHTML, complicatedHTML];
 
-    // Then
-    expect($(window)).to.be.equal(window);
-    expect($(document)).to.be.equal(document);
-    expect($(div) instanceof HTMLElement).to.be.true;
-    expect($($(div)) instanceof HTMLElement).to.be.true;
-    expect($(divs, true).length).to.be.equal(2);
-    expect($("#sandbox")).to.be.equal(this.el);
-    expect(this.el).to.be.equal(this.el);
-  });
-  it("should check `toArray` method", () => {
-    // Given
-    this.el.innerHTML = `<div>content1</div>
+		// Then
+		expect($(window)).to.be.equal(window);
+		expect($(document)).to.be.equal(document);
+		expect($(div) instanceof HTMLElement).to.be.true;
+		expect($($(div)) instanceof HTMLElement).to.be.true;
+		expect($(divs, true).length).to.be.equal(2);
+		expect($("#sandbox")).to.be.equal(this.el);
+		expect(this.el).to.be.equal(this.el);
+	});
+	it("should check `toArray` method", () => {
+		// Given
+		this.el.innerHTML = `<div>content1</div>
     <div>content2</div>
     <div>content3</div>`;
-    // When
-    const useSlice = Array.prototype.slice.call(this.el.childNodes);
+		// When
+		const useSlice = Array.prototype.slice.call(this.el.childNodes);
 
-    // Then
-    expect(toArray(this.el.childNodes)).to.be.eql(useSlice);
+		// Then
+		expect(toArray(this.el.childNodes)).to.be.eql(useSlice);
 	});
 
 	it("should roundNumber by round unit", () => {
