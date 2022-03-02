3a9aa69a4d81dca706c564602a54da76abf4c13b
malangfox <ahn3407@naver.com>
2022-03-02 16:05:22 +0900

    refactor: apply rules for naming, clarify access modifier

--- src/AnimationManager.ts
+++ src/AnimationManager.ts
@@ -11,24 +11,25 @@ function minMax(value: number, min: number, max: number): number {
 }
 
 export class AnimationManager {
+	public itm: InterruptManager;
+	public em: EventManager;
+	public axm: AxisManager;
 	private _raf;
 	private _animateParam: AnimationParam;
 	private _initialEasingPer: number;
 	private _prevEasingPer: number;
 	private _durationOffset: number;
-	private options: AxesOption;
-	public itm: InterruptManager;
-	public em: EventManager;
-	public axm: AxisManager;
+	private _options: AxesOption;
 
 	constructor({ options, itm, em, axm }) {
-		this.options = options;
+		this._options = options;
 		this.itm = itm;
 		this.em = em;
 		this.axm = axm;
 		this.animationEnd = this.animationEnd.bind(this);
 	}
-	getDuration(depaPos: Axis, destPos: Axis, wishDuration?: number) {
+
+	public getDuration(depaPos: Axis, destPos: Axis, wishDuration?: number): number {
 		let duration;
 		if (typeof wishDuration !== "undefined") {
 			duration = wishDuration;
@@ -37,47 +38,28 @@ export class AnimationManager {
 				destPos,
 				(v, k) => getDuration(
 					Math.abs(v - depaPos[k]),
-					this.options.deceleration),
+					this._options.deceleration),
 			);
 			duration = Object.keys(durations).reduce((max, v) => Math.max(max, durations[v]), -Infinity);
 		}
 		return minMax(
 			duration,
-			this.options.minimumDuration,
-			this.options.maximumDuration);
+			this._options.minimumDuration,
+			this._options.maximumDuration);
 	}
 
-	getDisplacement(velocity: number[]): number[] {
+	public getDisplacement(velocity: number[]): number[] {
 		const totalVelocity = Math.pow(velocity.reduce((total, v) => total + v * v, 0), 1 / (velocity.length));
-		const duration = Math.abs(totalVelocity / -this.options.deceleration);
+		const duration = Math.abs(totalVelocity / -this._options.deceleration);
 		return velocity.map(v => v / 2 * duration);
 	}
 
-	interpolate(displacement: number, threshold: number): number {
+	public interpolate(displacement: number, threshold: number): number {
 		const initSlope = this.easing(0.00001) / 0.00001;
 		return this.easing(displacement / (threshold * initSlope)) * threshold;
 	}
 
-	private createAnimationParam(pos: Axis, duration: number, option?: ChangeEventOption): AnimationParam {
-		const depaPos: Axis = this.axm.get();
-		const destPos: Axis = pos;
-		const inputEvent = option && option.event || null;
-		return {
-			depaPos,
-			destPos,
-			duration: minMax(
-				duration,
-				this.options.minimumDuration,
-				this.options.maximumDuration),
-			delta: this.axm.getDelta(depaPos, destPos),
-			inputEvent,
-			input: option && option.input || null,
-			isTrusted: !!inputEvent,
-			done: this.animationEnd,
-		};
-	}
-
-	stop(axes: string[], option?: ChangeEventOption) {
+	public stop(axes: string[], option?: ChangeEventOption): void {
 		if (this._animateParam && axes.length) {
 			const orgPos: Axis = this.axm.get(axes);
 			const pos: Axis = this.axm.map(orgPos,
@@ -92,7 +74,7 @@ export class AnimationManager {
 		}
 	}
 
-	getEventInfo(): ChangeEventOption {
+	public getEventInfo(): ChangeEventOption {
 		if (this._animateParam && this._animateParam.input && this._animateParam.inputEvent) {
 			return {
 				input: this._animateParam.input,
@@ -103,14 +85,14 @@ export class AnimationManager {
 		}
 	}
 
-	restore(option: ChangeEventOption) {
+	public restore(option: ChangeEventOption): void {
 		const pos: Axis = this.axm.get();
 		const destPos: Axis = this.axm.map(pos,
 			(v, opt) => Math.min(opt.range[1], Math.max(opt.range[0], v)));
 		this.animateTo(destPos, this.getDuration(pos, destPos), option);
 	}
 
-	animationEnd() {
+	public animationEnd(): void {
 		const beforeParam: ChangeEventOption = this.getEventInfo();
 		this._animateParam = null;
 
@@ -131,12 +113,158 @@ export class AnimationManager {
 			this.finish(!!beforeParam);
 		}
 	}
-	finish(isTrusted) {
+
+	public finish(isTrusted): void {
 		this._animateParam = null;
 		this.itm.setInterrupt(false);
 		this.em.triggerFinish(isTrusted);
 	}
-	private animateLoop(param: AnimationParam, complete: () => void) {
+
+	public getUserControll(param: AnimationParam) {
+		const userWish = param.setTo();
+		userWish.destPos = this.axm.get(userWish.destPos);
+		userWish.duration = minMax(
+			userWish.duration,
+			this._options.minimumDuration,
+			this._options.maximumDuration);
+		return userWish;
+	}
+
+	public animateTo(destPos: Axis, duration: number, option?: ChangeEventOption): void {
+		const param: AnimationParam = this.createAnimationParam(destPos, duration, option);
+		const depaPos = { ...param.depaPos };
+		const retTrigger = this.em.triggerAnimationStart(param);
+
+		// to control
+		const userWish = this.getUserControll(param);
+
+		// You can't stop the 'animationStart' event when 'circular' is true.
+		if (!retTrigger && this.axm.every(
+			userWish.destPos,
+			(v, opt) => isCircularable(v, opt.range, opt.circular as boolean[]))) {
+			console.warn("You can't stop the 'animation' event when 'circular' is true.");
+		}
+
+		if (retTrigger && !equal(userWish.destPos, depaPos)) {
+			const inputEvent = option && option.event || null;
+			this.animateLoop({
+				depaPos,
+				destPos: userWish.destPos,
+				duration: userWish.duration,
+				delta: this.axm.getDelta(depaPos, userWish.destPos),
+				isTrusted: !!inputEvent,
+				inputEvent,
+				input: option && option.input || null,
+			}, () => this.animationEnd());
+		}
+	}
+
+	public easing(p): number {
+		return p > 1 ? 1 : this._options.easing(p);
+	}
+
+	public setTo(pos: Axis, duration: number = 0) {
+		const axes: string[] = Object.keys(pos);
+		this.stop(axes);
+		const orgPos: Axis = this.axm.get(axes);
+
+		if (equal(pos, orgPos)) {
+			return this;
+		}
+		this.itm.setInterrupt(true);
+		let movedPos = filter(pos, (v, k) => orgPos[k] !== v);
+		if (!Object.keys(movedPos).length) {
+			return this;
+		}
+
+		movedPos = this.axm.map(movedPos, (v, opt) => {
+			const { range, circular } = opt;
+
+			if (circular && (circular[0] || circular[1])) {
+				return v;
+			} else {
+				return getInsidePosition(v, range, circular as boolean[]);
+			}
+		});
+
+		if (equal(movedPos, orgPos)) {
+			return this;
+		}
+
+		if (duration > 0) {
+			this.animateTo(movedPos, duration);
+		} else {
+			this.em.triggerChange(movedPos);
+			this.finish(false);
+		}
+
+		return this;
+	}
+
+	public setBy(pos: Axis, duration = 0) {
+		return this.setTo(
+			map(this.axm.get(Object.keys(pos)), (v, k) => v + pos[k]),
+			duration,
+		);
+	}
+
+	public updateAnimationPos(pos: Axis, restart: boolean): void {
+		const animateParam = this._animateParam;
+		if (!animateParam) {
+			return;
+		}
+		if (restart) {
+			this.setTo(pos, animateParam.duration - (new Date().getTime() - animateParam.startTime));
+		} else {
+			const currentPos = this.axm.get();
+			// When destination is changed, new delta should be calculated as remaining percent.
+			// For example, moving x:0, y:0 to x:200, y:200 and it has current easing percent of 92%. coordinate is x:184 and y:184
+			// If destination changes to x:300, y:300. xdelta:200, ydelta:200 changes to xdelta:116, ydelta:116 and use remaining easingPer as 100%, not 8% as previous.
+			// Therefore, original easingPer by time is kept. And divided by (1 - self._initialEasingPer) which means new total easing percent. Like calculating 8% as 100%.
+			this._initialEasingPer = this._prevEasingPer;
+			animateParam.delta = this.axm.getDelta(currentPos, pos);
+			animateParam.destPos = pos;
+		}
+	}
+
+	public updateAnimationDuration(duration: number,  restart: boolean): void {
+		const animateParam = this._animateParam;
+		if (!animateParam) {
+			return;
+		}
+		if (restart) {
+			this.setTo(animateParam.destPos, duration - animateParam.duration);
+		} else {
+			const diffTime = new Date().getTime() - animateParam.startTime;
+			const ratio = (diffTime + this._durationOffset) / animateParam.duration;
+			// Use durationOffset for keeping animation ratio after duration is changed.
+			// newRatio = (diffTime + newDurationOffset) / newDuration = oldRatio
+			// newDurationOffset = oldRatio * newDuration - diffTime
+			this._durationOffset = ratio * duration - diffTime;
+			animateParam.duration = duration;
+		}
+	}
+
+	private createAnimationParam(pos: Axis, duration: number, option?: ChangeEventOption): AnimationParam {
+		const depaPos: Axis = this.axm.get();
+		const destPos: Axis = pos;
+		const inputEvent = option && option.event || null;
+		return {
+			depaPos,
+			destPos,
+			duration: minMax(
+				duration,
+				this._options.minimumDuration,
+				this._options.maximumDuration),
+			delta: this.axm.getDelta(depaPos, destPos),
+			inputEvent,
+			input: option && option.input || null,
+			isTrusted: !!inputEvent,
+			done: this.animationEnd,
+		};
+	}
+
+	private animateLoop(param: AnimationParam, complete: () => void): void {
 		if (param.duration) {
 			let prevPos = param.depaPos;
 			this._initialEasingPer = 0;
@@ -230,7 +358,7 @@ export class AnimationManager {
 	}
 
 	private getRoundUnit(val: number, key: string) {
-		const roundUnit = this.options.round; // manual mode
+		const roundUnit = this._options.round; // manual mode
 		let minRoundUnit = null; // auto mode
 
 		// auto mode
@@ -244,128 +372,5 @@ export class AnimationManager {
 		}
 
 		return minRoundUnit || roundUnit;
-	}
-
-	getUserControll(param: AnimationParam) {
-		const userWish = param.setTo();
-		userWish.destPos = this.axm.get(userWish.destPos);
-		userWish.duration = minMax(
-			userWish.duration,
-			this.options.minimumDuration,
-			this.options.maximumDuration);
-		return userWish;
-	}
-
-	animateTo(destPos: Axis, duration: number, option?: ChangeEventOption) {
-		const param: AnimationParam = this.createAnimationParam(destPos, duration, option);
-		const depaPos = { ...param.depaPos };
-		const retTrigger = this.em.triggerAnimationStart(param);
-
-		// to control
-		const userWish = this.getUserControll(param);
-
-		// You can't stop the 'animationStart' event when 'circular' is true.
-		if (!retTrigger && this.axm.every(
-			userWish.destPos,
-			(v, opt) => isCircularable(v, opt.range, opt.circular as boolean[]))) {
-			console.warn("You can't stop the 'animation' event when 'circular' is true.");
-		}
-
-		if (retTrigger && !equal(userWish.destPos, depaPos)) {
-			const inputEvent = option && option.event || null;
-			this.animateLoop({
-				depaPos,
-				destPos: userWish.destPos,
-				duration: userWish.duration,
-				delta: this.axm.getDelta(depaPos, userWish.destPos),
-				isTrusted: !!inputEvent,
-				inputEvent,
-				input: option && option.input || null,
-			}, () => this.animationEnd());
-		}
-	}
-
-	easing(p) {
-		return p > 1 ? 1 : this.options.easing(p);
-	}
-
-	setTo(pos: Axis, duration: number = 0) {
-		const axes: string[] = Object.keys(pos);
-		this.stop(axes);
-		const orgPos: Axis = this.axm.get(axes);
-
-		if (equal(pos, orgPos)) {
-			return this;
-		}
-		this.itm.setInterrupt(true);
-		let movedPos = filter(pos, (v, k) => orgPos[k] !== v);
-		if (!Object.keys(movedPos).length) {
-			return this;
-		}
-
-		movedPos = this.axm.map(movedPos, (v, opt) => {
-			const { range, circular } = opt;
-
-			if (circular && (circular[0] || circular[1])) {
-				return v;
-			} else {
-				return getInsidePosition(v, range, circular as boolean[]);
-			}
-		});
-
-		if (equal(movedPos, orgPos)) {
-			return this;
-		}
-
-		if (duration > 0) {
-			this.animateTo(movedPos, duration);
-		} else {
-			this.em.triggerChange(movedPos);
-			this.finish(false);
-		}
-
-		return this;
-	}
-
-	setBy(pos: Axis, duration = 0) {
-		return this.setTo(
-			map(this.axm.get(Object.keys(pos)), (v, k) => v + pos[k]),
-			duration,
-		);
-	}
-
-	updateAnimationPos(pos: Axis, restart: boolean) {
-		const animateParam = this._animateParam;
-		if (!animateParam) {
-			return;
-		}
-		if (restart) {
-			this.setTo(pos, animateParam.duration - (new Date().getTime() - animateParam.startTime));
-		} else {
-			const currentPos = this.axm.get();
-			// currently remaining percent as 100%
-			//
-			this._initialEasingPer = this._prevEasingPer;
-			animateParam.delta = this.axm.getDelta(currentPos, pos);
-			animateParam.destPos = pos;
-		}
-	}
-
-	updateAnimationDuration(duration: number,  restart: boolean) {
-		const animateParam = this._animateParam;
-		if (!animateParam) {
-			return;
-		}
-		if (restart) {
-			this.setTo(animateParam.destPos, duration - animateParam.duration);
-		} else {
-			const diffTime = new Date().getTime() - animateParam.startTime;
-			const ratio = (diffTime + this._durationOffset) / animateParam.duration;
-			// Use durationOffset for keeping animation ratio after duration is changed.
-			// newRatio = (diffTime + newDurationOffset) / newDuration = oldRatio
-			// newDurationOffset = oldRatio * newDuration - diffTime
-			this._durationOffset = ratio * duration - diffTime;
-			animateParam.duration = duration;
-		}
 	}
 }
