4362650d43153854bed96d14198d2d0be1a13676
malangfox <ahn3407@naver.com>
2022-03-28 13:39:54 +0900

    demo: remove unnecessary files, update old demos

--- demo/src/pages/demos/3dcarousel.tsx
+++ demo/src/pages/demos/3dcarousel.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import Axes, { PanInput, WheelInput } from "../../../../src/index";
 import "../../css/demos/3dcarousel.css";
 
-function Carousel() {
+const Carousel = () => {
   useEffect(() => {
     var carousel = document.getElementById("carousel");
     var COUNT = 8;
@@ -13,85 +13,155 @@ function Carousel() {
     var axes = new Axes({
       rotate: {
         range: [0, 360],
-        circular: true
-      }
+        circular: true,
+      },
     });
 
     // 2. attach event handler
     axes.on({
-      "change": function(e) {
+      change: function (e) {
         var pos = e.pos;
 
-        carousel.style[Axes.TRANSFORM] = "translateZ(-253px) rotateY(" + (pos.rotate) + "deg)";
+        carousel.style[Axes.TRANSFORM] =
+          "translateZ(-253px) rotateY(" + pos.rotate + "deg)";
       },
-      "release": function (e) {
+      release: function (e) {
         var destPos = e.destPos;
 
-        e.setTo({"rotate": Math.round(destPos.rotate/ANGLE) * ANGLE }, 500);
-      }
+        e.setTo({ rotate: Math.round(destPos.rotate / ANGLE) * ANGLE }, 500);
+      },
     });
 
     // 3. Initialize a inputType and connect it
-    axes.connect("rotate", new PanInput("#carouselWrapper")).connect("rotate", new WheelInput("#carouselWrapper", {useNormalized: false}));
-  });
+    axes
+      .connect("rotate", new PanInput("#carouselWrapper"))
+      .connect(
+        "rotate",
+        new WheelInput("#carouselWrapper", { useNormalized: false })
+      );
+  }, []);
 
   return (
-    <div className="demobox" codepen="3dcarousel">
+    <div className="demobox">
       <p>You can create a carousel by controlling the animation.</p>
       <div id="carouselWrapper">
         <div className="list_container">
           <div id="carousel" style={{ transform: "translateZ(-253px)" }}>
             <figure style={{ transform: "rotateY(0deg) translateZ(253px)" }}>
-              <div id="list_cd" style={{ backgroundImage: `url(${require("@site/static/img/demos/3dcarousel/music1-min.jpg").default}` }}>
+              <div
+                id="list_cd"
+                style={{
+                  backgroundImage: `url(${
+                    require("@site/static/img/demos/3dcarousel/music1-min.jpg")
+                      .default
+                  }`,
+                }}
+              >
                 <div id="list_cd_hole"></div>
               </div>
               <div id="list_cd_title">Too Much</div>
               <div id="list_cd_artist">Loco</div>
             </figure>
             <figure style={{ transform: "rotateY(45deg) translateZ(253px)" }}>
-              <div id="list_cd" style={{ backgroundImage: `url(${require("@site/static/img/demos/3dcarousel/music2-min.jpg").default}` }}>
+              <div
+                id="list_cd"
+                style={{
+                  backgroundImage: `url(${
+                    require("@site/static/img/demos/3dcarousel/music2-min.jpg")
+                      .default
+                  }`,
+                }}
+              >
                 <div id="list_cd_hole"></div>
               </div>
               <div id="list_cd_title">Woo ah</div>
               <div id="list_cd_artist">Crush</div>
             </figure>
             <figure style={{ transform: "rotateY(90deg) translateZ(253px)" }}>
-              <div id="list_cd" style={{ backgroundImage: `url(${require("@site/static/img/demos/3dcarousel/music3-min.jpg").default}` }}>
+              <div
+                id="list_cd"
+                style={{
+                  backgroundImage: `url(${
+                    require("@site/static/img/demos/3dcarousel/music3-min.jpg")
+                      .default
+                  }`,
+                }}
+              >
                 <div id="list_cd_hole"></div>
               </div>
               <div id="list_cd_title">Man In The Mirror</div>
               <div id="list_cd_artist">Micheal Jackson</div>
             </figure>
             <figure style={{ transform: "rotateY(135deg) translateZ(253px)" }}>
-              <div id="list_cd" style={{ backgroundImage: `url(${require("@site/static/img/demos/3dcarousel/music4-min.jpg").default}` }}>
+              <div
+                id="list_cd"
+                style={{
+                  backgroundImage: `url(${
+                    require("@site/static/img/demos/3dcarousel/music4-min.jpg")
+                      .default
+                  }`,
+                }}
+              >
                 <div id="list_cd_hole"></div>
               </div>
               <div id="list_cd_title">Adult</div>
               <div id="list_cd_artist">GiriBoy</div>
             </figure>
             <figure style={{ transform: "rotateY(180deg) translateZ(253px)" }}>
-              <div id="list_cd" style={{ backgroundImage: `url(${require("@site/static/img/demos/3dcarousel/music5-min.jpg").default}` }}>
+              <div
+                id="list_cd"
+                style={{
+                  backgroundImage: `url(${
+                    require("@site/static/img/demos/3dcarousel/music5-min.jpg")
+                      .default
+                  }`,
+                }}
+              >
                 <div id="list_cd_hole"></div>
               </div>
               <div id="list_cd_title">Always Awake</div>
               <div id="list_cd_artist">Beenzino</div>
             </figure>
             <figure style={{ transform: "rotateY(225deg) translateZ(253px)" }}>
-              <div id="list_cd" style={{ backgroundImage: `url(${require("@site/static/img/demos/3dcarousel/music6-min.jpg").default}` }}>
+              <div
+                id="list_cd"
+                style={{
+                  backgroundImage: `url(${
+                    require("@site/static/img/demos/3dcarousel/music6-min.jpg")
+                      .default
+                  }`,
+                }}
+              >
                 <div id="list_cd_hole"></div>
               </div>
               <div id="list_cd_title">City Burns</div>
               <div id="list_cd_artist">Andra Day</div>
             </figure>
             <figure style={{ transform: "rotateY(270deg) translateZ(253px)" }}>
-              <div id="list_cd" style={{ backgroundImage: `url(${require("@site/static/img/demos/3dcarousel/music7-min.jpg").default}` }}>
+              <div
+                id="list_cd"
+                style={{
+                  backgroundImage: `url(${
+                    require("@site/static/img/demos/3dcarousel/music7-min.jpg")
+                      .default
+                  }`,
+                }}
+              >
                 <div id="list_cd_hole"></div>
               </div>
               <div id="list_cd_title">Get Some Air</div>
               <div id="list_cd_artist">Gary</div>
             </figure>
             <figure style={{ transform: "rotateY(315deg) translateZ(253px)" }}>
-              <div id="list_cd" style={{ backgroundImage: `url(${require("@site/static/img/demos/3dcarousel/music8-min.jpg").default}` }}>
+              <div
+                id="list_cd"
+                style={{
+                  backgroundImage: `url(${
+                    require("@site/static/img/demos/3dcarousel/music8-min.jpg")
+                      .default
+                  }`,
+                }}
+              >
                 <div id="list_cd_hole"></div>
               </div>
               <div id="list_cd_title">Hold Me Tight</div>
@@ -102,6 +172,6 @@ function Carousel() {
       </div>
     </div>
   );
-}
+};
 
 export default Carousel;
--- demo/src/pages/demos/axes.tsx
+++ demo/src/pages/demos/axes.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect } from "react";
 
-import Axes, { PanInput, PinchInput, MoveKeyInput, WheelInput } from "../../../../src/index";
-import Icon from '../../../static/img/demos/axes/logo.svg';
+import Axes, {
+  PanInput,
+  PinchInput,
+  MoveKeyInput,
+  WheelInput,
+} from "../../../../src/index";
+import Icon from "../../../static/img/demos/axes/logo.svg";
 import "../../css/demos/axes.css";
 
-function AxesDemo() {
+const AxesDemo = () => {
   useEffect(() => {
     /* AxesGridView for test */
     function AxesGridView(container, axisX, axisY, axisZoom) {
@@ -13,35 +18,41 @@ function AxesDemo() {
       this.axisY = axisY;
       this.axisZoom = axisZoom;
       this.canvas = document.createElement("canvas");
-      this.canvas.width = (axisX.range[1] - axisX.range[0]) + axisX.bounce[0] + axisX.bounce[1];
-      this.canvas.height = (axisY.range[1] - axisY.range[0]) + axisY.bounce[0] + axisY.bounce[1];
+      this.canvas.width =
+        axisX.range[1] - axisX.range[0] + axisX.bounce[0] + axisX.bounce[1];
+      this.canvas.height =
+        axisY.range[1] - axisY.range[0] + axisY.bounce[0] + axisY.bounce[1];
       this.ctx = this.canvas.getContext("2d");
       this.container.appendChild(this.canvas);
 
       this.render(axisX.range[0], axisX.range[0]);
     }
-    AxesGridView.prototype.render = function(x, y, zoom) {
-
+    AxesGridView.prototype.render = function (x, y, zoom) {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
       this._renderGrid();
       this._renderPoint(x, y, zoom);
     };
 
-    AxesGridView.prototype._renderGrid = function() {
-      var ctx = this.ctx;
-      var canvas = this.canvas;
-      var range = [
+    AxesGridView.prototype._renderGrid = function () {
+      const ctx = this.ctx;
+      const canvas = this.canvas;
+      const range = [
         this.axisX.bounce[0],
         this.axisY.bounce[0],
         canvas.width - this.axisX.bounce[0],
-        canvas.height - this.axisY.bounce[0]
+        canvas.height - this.axisY.bounce[0],
       ];
 
       ctx.fillStyle = "#e6e6e6";
       ctx.fillRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = "#fff";
-      ctx.fillRect(range[0], range[1], range[2] - this.axisX.bounce[1], range[3] - this.axisY.bounce[1]);
+      ctx.fillRect(
+        range[0],
+        range[1],
+        range[2] - this.axisX.bounce[1],
+        range[3] - this.axisY.bounce[1]
+      );
       ctx.beginPath();
       ctx.lineWidth = 0.25;
 
@@ -69,13 +80,19 @@ function AxesDemo() {
       ctx.moveTo(range[0], range[1]);
       ctx.lineTo(canvas.width - this.axisX.bounce[1], range[1]);
       ctx.moveTo(range[0], canvas.height - this.axisY.bounce[1]);
-      ctx.lineTo(canvas.width - this.axisX.bounce[1], canvas.height - this.axisY.bounce[1]);
+      ctx.lineTo(
+        canvas.width - this.axisX.bounce[1],
+        canvas.height - this.axisY.bounce[1]
+      );
 
       // vertical
       ctx.moveTo(range[0], range[1]);
       ctx.lineTo(range[0], canvas.height - this.axisY.bounce[1]);
       ctx.moveTo(canvas.width - this.axisX.bounce[1], range[1]);
-      ctx.lineTo(canvas.width - this.axisX.bounce[1], canvas.height - this.axisY.bounce[1]);
+      ctx.lineTo(
+        canvas.width - this.axisX.bounce[1],
+        canvas.height - this.axisY.bounce[1]
+      );
 
       ctx.strokeStyle = "#888";
       ctx.stroke();
@@ -85,51 +102,82 @@ function AxesDemo() {
       ctx.fillStyle = "black";
       ctx.textAlign = "left";
       ctx.textBaseline = "bottom";
-      ctx.fillText("(panX: " + this.axisX.range[0] + ", panY: " + this.axisY.range[0] + ")", range[0], range[1]);
+      ctx.fillText(
+        "(panX: " +
+          this.axisX.range[0] +
+          ", panY: " +
+          this.axisY.range[0] +
+          ")",
+        range[0],
+        range[1]
+      );
       ctx.textAlign = "right";
       ctx.textBaseline = "hanging";
-      ctx.fillText("(panX: " + this.axisX.range[1] + ", panY: " + this.axisY.range[1] + ")", range[2], range[3]);
+      ctx.fillText(
+        "(panX: " +
+          this.axisX.range[1] +
+          ", panY: " +
+          this.axisY.range[1] +
+          ")",
+        range[2],
+        range[3]
+      );
     };
 
-    AxesGridView.prototype._renderPoint = function(x, y, zoom) {
+    AxesGridView.prototype._renderPoint = function (x, y, zoom) {
       zoom = Number(zoom);
       var ctx = this.ctx;
       ctx.globalAlpha = 0.7;
       ctx.arc(
         this.axisX.bounce[0] + x,
         this.axisY.bounce[0] + y,
-        10 * zoom, 0, 2 * Math.PI, false);
+        10 * zoom,
+        0,
+        2 * Math.PI,
+        false
+      );
       ctx.fillStyle = "red";
       ctx.fill();
       ctx.globalAlpha = 1;
 
-      ctx.font = "bolder " + (zoom * 10) + "px Arial";
+      ctx.font = "bolder " + zoom * 10 + "px Arial";
       ctx.textAlign = "center";
       ctx.textBaseline = "bottom";
-      ctx.fillText("(panX: " + x.toFixed(0) + ", panY: " + y.toFixed(0) + ", zoom: " + zoom.toFixed(2) + ")",
+      ctx.fillText(
+        "(panX: " +
+          x.toFixed(0) +
+          ", panY: " +
+          y.toFixed(0) +
+          ", zoom: " +
+          zoom.toFixed(2) +
+          ")",
         this.axisX.bounce[0] + x,
-        this.axisY.bounce[0] + y - 10 * zoom);
+        this.axisY.bounce[0] + y - 10 * zoom
+      );
     };
 
     // fit size
-    function fit() {
-      const baseSize = document.getElementById("inputTypeWrapper").getBoundingClientRect().width;
-      const size = baseSize < 600 ?
-        [baseSize - 40, (baseSize/3*2) - 40] :
-        [baseSize/2 - 50, (baseSize/3*2)/2];
+    const fit = () => {
+      const baseSize = document
+        .getElementById("inputTypeWrapper")
+        .getBoundingClientRect().width;
+      const size =
+        baseSize < 600
+          ? [baseSize - 40, (baseSize / 3) * 2 - 40]
+          : [baseSize / 2 - 50, ((baseSize / 3) * 2) / 2];
 
       const uiWrapper = document.getElementById("uiWrapper");
-      uiWrapper.style.width = (size[0] + 40)+ "px";
+      uiWrapper.style.width = size[0] + 40 + "px";
       uiWrapper.style.height = size[1] + "px";
       if (baseSize > 600) {
         uiWrapper.style.marginRight = "20px";
         uiWrapper.style.marginTop = "20px";
       }
       return size;
-    }
+    };
 
     // stop blicking
-    function stopBlinking(event) {
+    const stopBlinking = (event?) => {
       if (event) {
         if (!event.delta.panX && !event.delta.panY) {
           pan.classList.remove("blinking");
@@ -141,7 +189,7 @@ function AxesDemo() {
         pan.classList.remove("blinking");
         setTimeout(() => zoom.classList.remove("blinking"), 300);
       }
-    }
+    };
 
     const SUPPORT_TOUCH = "ontouchstart" in window;
     const delegateTarget = document.getElementById("delegateTarget");
@@ -153,80 +201,98 @@ function AxesDemo() {
     const zoom = inputType.querySelector(".zoom");
 
     // 1. Initialize eg.Axes
-    const axes = new Axes({
-      panX: {
-        range: [0, size[0]],
-        bounce: 20
+    const axes = new Axes(
+      {
+        panX: {
+          range: [0, size[0]],
+          bounce: 20,
+        },
+        panY: {
+          range: [0, size[1]],
+          bounce: 20,
+        },
+        zoom: {
+          range: [1, 5],
+          bounce: 1,
+        },
       },
-      panY: {
-        range: [0, size[1]],
-        bounce: 20
-      },
-      zoom: {
-        range: [1, 5],
-        bounce: 1
+      {
+        minimumDuration: 300,
       }
-    }, {
-      minimumDuration: 300
-    });
+    );
 
-    if (document.getElementById("grid").children.length) {
-      return;
-    }
     // draw grid
-    const gridView = new AxesGridView(document.getElementById("grid"),
+    const gridView = new AxesGridView(
+      document.getElementById("grid"),
       axes.axis.panX,
       axes.axis.panY,
-      axes.axis.zoom);
+      axes.axis.zoom
+    );
 
     // 2. attach event handler
     axes.on({
-      "hold": event => pan.classList.add("blinking"),
-      "change": function (e) {
-        var pos = e.pos;
-        var delta = e.delta;
-        var holding = e.holding;
+      hold: (event) => pan.classList.add("blinking"),
+      change: (e) => {
+        const pos = e.pos;
+        const delta = e.delta;
+        const holding = e.holding;
 
         if (delta.panX || delta.panY) {
-          pan.textContent =
-            `panX: ${(+pos.panX.toFixed(0))}, panY: ${(+pos.panY.toFixed(0))}`;
-            if (e.inputEvent && (e.inputEvent.type === "keydown" || e.inputEvent.type === "keyup")) {
-                pan.classList.add("keyboard");
-            } else {
-              pan.classList.remove("keyboard");
-            }
-            holding && !pan.classList.contains("blinking") &&
+          pan.textContent = `panX: ${+pos.panX.toFixed(
+            0
+          )}, panY: ${+pos.panY.toFixed(0)}`;
+          if (
+            e.inputEvent &&
+            (e.inputEvent.type === "keydown" || e.inputEvent.type === "keyup")
+          ) {
+            pan.classList.add("keyboard");
+          } else {
+            pan.classList.remove("keyboard");
+          }
+          holding &&
+            !pan.classList.contains("blinking") &&
             pan.classList.add("blinking");
         }
         if (delta.zoom) {
           zoom.textContent = `zoom: ${pos.zoom.toFixed(2)}`;
-          !zoom.classList.contains("blinking") && zoom.classList.add("blinking");
+          !zoom.classList.contains("blinking") &&
+            zoom.classList.add("blinking");
         }
         gridView.render(pos.panX, pos.panY, pos.zoom);
-        ui.style[Axes.TRANSFORM] =
-          `translate3d(${pos.panX}px, ${pos.panY}px, 0) scale(${pos.zoom})`;
+        ui.style[
+          Axes.TRANSFORM
+        ] = `translate3d(${pos.panX}px, ${pos.panY}px, 0) scale(${pos.zoom})`;
       },
-      "release": event => stopBlinking(event),
-      "animationEnd": () => stopBlinking()
+      release: (event) => stopBlinking(event),
+      animationEnd: () => stopBlinking(),
     });
 
     // 3. Initialize inputTypes and connect it
-    axes.connect("panX panY", new PanInput(delegateTarget))
-    axes.connect("panX panY", new MoveKeyInput(delegateTarget, {
-      scale: [5, -5]
-    }))
-      .connect("zoom", SUPPORT_TOUCH ?
-        new PinchInput(delegateTarget) :
-        new WheelInput(delegateTarget));
+    axes.connect("panX panY", new PanInput(delegateTarget));
+    axes
+      .connect(
+        "panX panY",
+        new MoveKeyInput(delegateTarget, {
+          scale: [5, -5],
+        })
+      )
+      .connect(
+        "zoom",
+        SUPPORT_TOUCH
+          ? new PinchInput(delegateTarget)
+          : new WheelInput(delegateTarget)
+      );
 
     // 4. move to position
-    axes.setTo({panX: size[0]/2 + 20, panY: size[1]/2});
-  });
-
+    axes.setTo({ panX: size[0] / 2 + 20, panY: size[1] / 2 });
+  }, []);
 
   return (
-    <div className="demobox" codepen="axes">
-      <p>You can change the value of the axis and apply it to the UI via the touch screen, mouse, or various other inputs.</p>
+    <div className="demobox">
+      <p>
+        You can change the value of the axis and apply it to the UI via the
+        touch screen, mouse, or various other inputs.
+      </p>
       <div id="inputTypeWrapper">
         <div id="inputType">
           <div className="pan"></div>
@@ -236,7 +302,7 @@ function AxesDemo() {
       <div id="delegateTarget">
         <div id="uiWrapper">
           <div className="ui">
-            <Icon style={{ height: "75px" }}/>
+            <Icon style={{ height: "75px" }} />
           </div>
         </div>
         <div id="grid"></div>
@@ -244,6 +310,6 @@ function AxesDemo() {
       <div style={{ clear: "both" }}></div>
     </div>
   );
-}
+};
 
 export default AxesDemo;
