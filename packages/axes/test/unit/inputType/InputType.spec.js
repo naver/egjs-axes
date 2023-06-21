91d068ff27969cf2dbc97121bd72b4ac99ef04fa
malangfox <ahn3407@naver.com>
2023-06-22 02:59:23 +0900

    test: add eventInput unit test

--- packages/axes/test/unit/inputType/InputType.spec.js
+++ packages/axes/test/unit/inputType/InputType.spec.js
@@ -1,10 +1,14 @@
+import Axes from "../../../src/Axes.ts";
 import { MouseEventInput } from "../../../src/eventInput/MouseEventInput";
 import { PointerEventInput } from "../../../src/eventInput/PointerEventInput";
 import { TouchEventInput } from "../../../src/eventInput/TouchEventInput";
 import { TouchMouseEventInput } from "../../../src/eventInput/TouchMouseEventInput";
+import { PanInput } from "../../../src/inputType/PanInput.ts";
 
 import InputInjector from "inject-loader!../../../src/inputType/InputType";
 import EventInjector from "inject-loader!../../../src/eventInput/EventInput";
+
+import TestHelper from "./TestHelper";
 
 describe("InputType", () => {
   describe("SUPPORT_TOUCH mocking", () => {
@@ -81,6 +85,73 @@ describe("InputType", () => {
       inputType = [];
       // Then
       expect(MockInputInjector.convertInputType(inputType)).to.be.null;
+    });
+  });
+
+  describe("eventInput", () => {
+    [["mouse"], ["touch"], ["touch", "mouse"]].forEach((inputType) => {
+      it(`should recognize the correct event contained in the inputType (inputType: ${inputType}, event: mouse)`, async () => {
+        // Given
+        const MockEventInjector = EventInjector();
+        MockEventInjector.SUPPORT_TOUCH = true;
+        const change = sinon.spy();
+        const el = sandbox();
+        const input = new PanInput(el, {
+          inputType,
+        });
+        const inst = new Axes(
+          {
+            x: {
+              range: [0, 100],
+            },
+          }
+        );
+        inst.connect("x", input);
+        inst.on("change", change);
+
+        // When
+        await TestHelper.dispatchDrag(
+          el,
+          { left: 0, top: 0 },
+          { left: 100, top: 0 },
+          { duration: 100, interval: 50 }
+        );
+
+        // Then
+        expect(change.called).to.be.equal(inputType.indexOf("mouse") > -1);
+      });
+
+      it(`should recognize the correct event contained in the inputType (inputType: ${inputType}, event: touch)`, (done) => {
+        // Given
+        const MockEventInjector = EventInjector();
+        MockEventInjector.SUPPORT_TOUCH = true;
+        const change = sinon.spy();
+        const el = sandbox();
+        const input = new PanInput(el, {
+          inputType,
+        });
+        const inst = new Axes(
+          {
+            x: {
+              range: [0, 100],
+            },
+          }
+        );
+        inst.connect("x", input);
+        inst.on("change", change);
+
+        // When
+        Simulator.gestures.pan(el, {
+          pos: [0, 0],
+          deltaX: 100,
+          duration: 1500,
+          easing: "linear",
+        }, () => {
+          // Then
+          expect(change.called).to.be.equal(inputType.indexOf("touch") > -1);
+          done();
+        });
+      });
     });
   });
 });
