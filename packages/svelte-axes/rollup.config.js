3fc40a2458c72982976ac8b8f15708d9451a4b3f
malangfox <seonghyun.ahn@navercorp.com>
2024-07-31 19:23:46 +0900

    chore: update packages versions

--- packages/svelte-axes/rollup.config.js
+++ packages/svelte-axes/rollup.config.js
@@ -1,33 +1,37 @@
 import buildHelper from "@egjs/build-helper";
 import svelte from "rollup-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
+import nodeResolve from "@rollup/plugin-node-resolve";
 
 const defaultOptions = {
   tsconfig: "",
   commonjs: true,
-  external: {
-    svelte: "svelte",
-  },
-  plugins: [
-    svelte({
+	external: {
+		svelte: "svelte",
+	},
+	plugins: [
+		svelte({
       preprocess: sveltePreprocess(),
     }),
-  ],
+    nodeResolve({
+      browser: true
+    }),
+	],
 };
 
 export default buildHelper([
-  {
-    ...defaultOptions,
+	{
+		...defaultOptions,
     input: "./src/svelte-axes/index.ts",
-    output: "dist/axes.cjs.js",
-    format: "cjs",
+		output: "dist/axes.cjs.js",
+		format: "cjs",
     exports: "named",
-  },
-  {
-    ...defaultOptions,
+	},
+	{
+		...defaultOptions,
     input: "./src/svelte-axes/index.ts",
-    output: "dist/axes.esm.js",
-    format: "es",
-    exports: "named",
-  },
+		output: "dist/axes.esm.js",
+		format: "es",
+		exports: "named",
+	},
 ]);
